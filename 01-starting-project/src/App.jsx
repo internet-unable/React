7eb201a01304df8f4065df7ccc33625b2a06a7cc
internet-unable <k_moiseenko@icloud.com>
2024-11-01 13:41:29 +0200

    Update for App.jsx
    Created my firt custom component.
    Learned how to work with props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,19 +1,59 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
+
+const reactDesc = ['Fundamental', 'Crucial', 'Core'];
+function getRandomInd(max) {
+    return Match.floor(Math.random() * max);
+}
+
+function Header() {
+    return (
+        <header>
+            <img src={reactImg} alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                {reactDesc.getRandomInd(3)} React concepts you will need for almost any app you are going to build!
+            </p>
+        </header>
+    );
+}
+
+function CoreConcept({title = 'Title', desc, image}) {
+    // default value for title
+    return (
+        <li>
+            <img src={image} alt={`${title}. ${desc}`} />
+            <h3>{title}</h3>
+            <p>{desc}</p>
+        </li>
+    );
+}
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main>
+                <section id="core-concepts">
+                    <h2>Time to get started!</h2>
+                    <ul>
+                        <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            desc={CORE_CONCEPTS[0].desc}
+                            image={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[1].title}
+                            desc={CORE_CONCEPTS[1].desc}
+                            image={CORE_CONCEPTS[1].image}
+                        />
+                        <CoreConcept {...CORE_CONCEPTS[2]}/>
+                        <CoreConcept {...CORE_CONCEPTS[3]}/>
+                    </ul>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
