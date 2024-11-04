02dcbcf3e437df90840ea1e2dda88a52a50d870f
internet-unable <k_moiseenko@icloud.com>
2024-11-04 17:28:46 +0200

    Learned how to dynamicly render elements

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -19,10 +19,7 @@ function App() {
                 <section id="core-concepts">
                     <h2>Time to get started!</h2>
                     <ul>
-                        <CoreConcept {...CORE_CONCEPTS[0]}/>
-                        <CoreConcept {...CORE_CONCEPTS[1]}/>
-                        <CoreConcept {...CORE_CONCEPTS[2]}/>
-                        <CoreConcept {...CORE_CONCEPTS[3]}/>
+                        {CORE_CONCEPTS.map((item) => <CoreConcept {...item}/>)}
                     </ul>
                 </section>
 
