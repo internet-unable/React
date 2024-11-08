bc98dac7243691ae56a13d1ee56e9c90c972ffe2
internet-unable <k_moiseenko@icloud.com>
2024-11-08 14:56:10 +0200

    Added blueprint for result table

--- 03-investment-calculator/src/App.jsx
+++ 03-investment-calculator/src/App.jsx
@@ -25,8 +25,27 @@ function App() {
                 </div>
             </section>
 
-            <section>
-                Result
+            <section id="result">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Year</th>
+                            <th>Investment value</th>
+                            <th>Interest (Year)</th>
+                            <th>Total interest</th>
+                            <th>Invested Capital</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>$10,850</td>
+                            <td>$550</td>
+                            <td>$550</td>
+                            <td>$10,300</td>
+                        </tr>
+                    </tbody>
+                </table>
             </section>
         </>
     )
