083d27036c859e1be0bcfd076c0df6c44e0a3562
internet-unable <k_moiseenko@icloud.com>
2024-11-08 16:56:45 +0200

    Update Results component
    Some structure update.
    Added calculations

--- 03-investment-calculator/src/components/Results/Results.jsx
+++ 03-investment-calculator/src/components/Results/Results.jsx
@@ -1,30 +1,35 @@
 import {formatter} from '../../util/investment.js';
 
 export default function Results({ list }) {
+    const initialInvestment = list[0].valueEndOfYear - list[0].interest - list[0].annualInvestment;
+
     return (
-        <section id="result">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Year</th>
-                        <th>Investment value</th>
-                        <th>Interest (Year)</th>
-                        <th>Total interest</th>
-                        <th>Invested Capital</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {list.map(item => (
+        <table id="result">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment value</th>
+                    <th>Interest (Year)</th>
+                    <th>Total interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {list.map(item => {
+                    const totalInterest = item.valueEndOfYear - (item.annualInvestment * item.year) - initialInvestment;
+                    const totalAmountInvested = item.valueEndOfYear - totalInterest;
+
+                    return (
                         <tr key={item.year}>
                             <td>{item.year}</td>
                             <td>{formatter.format(item.valueEndOfYear)}</td>
                             <td>{formatter.format(item.interest)}</td>
-                            <td>$550</td>
-                            <td>$10,300</td>
+                            <td>{formatter.format(totalInterest)}</td>
+                            <td>{formatter.format(totalAmountInvested)}</td>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </section>
+                    )
+                })}
+            </tbody>
+        </table>
     );
 }
