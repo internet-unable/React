9bdb8d4bbf04363f080fd1f9b74e5213a153b3e1
internet-unable <k_moiseenko@icloud.com>
2024-11-11 13:37:05 +0200

    Applied Tailwind CSS on Header component

--- 04-react-art/src/components/Header.jsx
+++ 04-react-art/src/components/Header.jsx
@@ -2,10 +2,14 @@ import logo from '../assets/logo.png';
 
 export default function Header() {
     return (
-        <header>
-            <img src={logo} alt="A canvas" />
-            <h1 className="text-3xl font-bold underline">ReactArt</h1>
-            <p>A community of artists and art-lovers.</p>
+        <header className="flex flex-col items-center justify-center my-8 md:mb-16">
+            <img
+                className="object-contain mb-8 w-44 h-44"
+                src={logo}
+                alt="A canvas"
+            />
+            <h1 className="font-pacifico text-2xl md:text-3xl font-semibold uppercase text-center text-header-h1 m-0">ReactArt</h1>
+            <p className="text-center text-header-p m-0">A community of artists and art-lovers.</p>
         </header>
     );
 }
--- 04-react-art/tailwind.config.js
+++ 04-react-art/tailwind.config.js
@@ -1,10 +1,24 @@
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        fontFamily: {
+            pacifico: "Pacifico, cursive",
+        },
+        fontSize: {
+            "2xl": [
+                "1.5rem",
+                {
+                    letterSpacing: "0.4em",
+                },
+            ],
+            "3xl": "2.25rem"
+        },
+        extend: {
+            colors: {
+                "header-h1": "#9a3412",
+                "header-p": "#a39191",
+            },
+        },
     },
     plugins: [],
 };
