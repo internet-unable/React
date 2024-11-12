45673f21a09d76da0ab5b8f605c8e8eb25a84943
internet-unable <k_moiseenko@icloud.com>
2024-11-12 15:05:01 +0200

    Update for TimerChallenge
    Updated this component to use Ref as container for timer

--- 05-react-countdown/src/components/TimerChallenge/TimerChallenge.jsx
+++ 05-react-countdown/src/components/TimerChallenge/TimerChallenge.jsx
@@ -1,14 +1,37 @@
+import { useState, useRef } from "react";
+
 export default function TimerChallenge({title, targetTime}) {
+    const [timerStarted, setTimerStarted] = useState();
+    const [timerExpired, setTimerExpired] = useState(false);
+    const timer = useRef();
+
+    function handleStart() {
+        timer.current = setTimeout(() => {
+            setTimerExpired(true);
+        }, targetTime * 1000);
+    }
+
+    function handleStop() {
+        clearTimeout(timer.current);
+    }
+
     return(
         <section className="challenge">
             <h2>{title}</h2>
+
+            {timerExpired && <p>You lost!</p>}
+
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 && 's'}
             </p>
             <p>
-                <button type="button">Start/Stop challenge</button>
+                <button type="button" onClick={timerStarted ? handleStop: handleStart}>
+                    {timerStarted ? 'Stop' : 'Start'} challenge
+                </button>
             </p>
-            <p className="">Time is running.../Timer inactive</p>
+            <p className={timerStarted ? 'active' : ''}>
+                {timerStarted ? 'Time is running...' : 'Timer inactive'}
+            </p>
         </section>
     );
 }
