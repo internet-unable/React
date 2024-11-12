aaff3380822fa8ad7ca78bae1ed7169cb0ddcce9
internet-unable <k_moiseenko@icloud.com>
2024-11-12 09:53:58 +0200

    Apply two-way binding

--- 05-react-countdown/src/components/Player.jsx
+++ 05-react-countdown/src/components/Player.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+    const [enteredPlayerName, setEnteredPlayerName] = useState('');
+    const [isNameSet, setIsNameSet] = useState(false);
+
+    function handleInputChange(event) {
+        setIsNameSet(false);
+        setEnteredPlayerName(event.target.value);
+    }
+
+    function handleBtnClick() {
+        setIsNameSet(true);
+    }
+
+    return (
+        <section id="player">
+            <h2>Welcome {isNameSet ? enteredPlayerName : 'unknown entity'}</h2>
+            <p>
+                <input type="text" onChange={handleInputChange} value={enteredPlayerName} />
+                <button type="button" onClick={handleBtnClick}>Set Name</button>
+            </p>
+        </section>
+    );
 }
