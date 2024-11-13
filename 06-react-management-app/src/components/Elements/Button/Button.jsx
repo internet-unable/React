09e255f1cf12e9021f950d1d6a3383f0e02a67cc
internet-unable <k_moiseenko@icloud.com>
2024-11-13 21:25:32 +0200

    Update for Button component
    Add basic styles for different types of button

--- 06-react-management-app/src/components/Elements/Button/Button.jsx
+++ 06-react-management-app/src/components/Elements/Button/Button.jsx
@@ -1,5 +1,19 @@
-export default function Button({type = 'button', children, onClick}) {
+export default function Button({btnType = 'button', children, onClick}) {
+    const typeButtonStyles = 'px-4 py-2 bg-black text-white rounded';
+    const typeTextStyles = 'text-black';
+    let finalStyles = typeButtonStyles;
+
+    if (btnType === 'text') {
+        finalStyles = typeTextStyles;
+    }
+
     return(
-        <button type={type} onClick={onClick}>{children}</button>
+        <button
+            type="button"
+            className={finalStyles}
+            onClick={onClick}
+        >
+            {children}
+        </button>
     );
 }
