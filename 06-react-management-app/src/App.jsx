3b5eae624e8e455ed5aa100dcca07ed88340bc1d
internet-unable <k_moiseenko@icloud.com>
2024-11-13 18:44:55 +0200

    Update for ProjectPanel component
    Moved code inside component

--- 06-react-management-app/src/App.jsx
+++ 06-react-management-app/src/App.jsx
@@ -1,6 +1,7 @@
 import SideMenu from "./components/SideMenu/SideMenu.jsx";
 import NoProjectSelectedPanel from "./components/Panels/NoProjectSelectedPanel/NoProjectSelectedPanel.jsx";
 import CreateProjectPanel from "./components/Panels/CreateProjectPanel/CreateProjectPanel.jsx";
+import ProjectPanel from "./components/Panels/ProjectPanel/ProjectPanel.jsx";
 
 function App() {
     return (
@@ -8,44 +9,7 @@ function App() {
             <SideMenu />
             <NoProjectSelectedPanel />
             <CreateProjectPanel />
-
-            <section id="project-panel">
-                <ul>
-                    <li>
-                        <h1>Learning react</h1>
-                    </li>
-                    <li>
-                        <button type="button">Delete</button>
-                    </li>
-                </ul>
-                <p>Dec 29, 2024</p>
-                <p>Learn React from group up. Start with the basics, finish with advanced knowledge</p>
-                <hr />
-
-                <section id="project-panel">
-                    <h2>Tasks</h2>
-                    <div>
-                        <div>
-                            <input type="text" />
-                            <button type="button">Add task</button>
-                        </div>
-                        <ul>
-                            <li>
-                                <span>Learn the basics</span>
-                                <button type="button">Clear</button>
-                            </li>
-                            <li>
-                                <span>Learn advanced concepts</span>
-                                <button type="button">Clear</button>
-                            </li>
-                            <li>
-                                <span>Practice, practice, practice</span>
-                                <button type="button">Clear</button>
-                            </li>
-                        </ul>
-                    </div>
-                </section>
-            </section>
+            <ProjectPanel />
         </>
     );
 }
--- 06-react-management-app/src/components/Panels/ProjectPanel/ProjectPanel.jsx
+++ 06-react-management-app/src/components/Panels/ProjectPanel/ProjectPanel.jsx
@@ -1,5 +1,46 @@
-export default function ProjectPanel() {
-    return(
-        <p>This is component for project-panel</p>
+import Button from "../../Elements/Button/Button.jsx";
+import Input from "../../Elements/Input/Input.jsx";
+
+export default function ProjectPanel({title, dueDate, desc}) {
+    return (
+        <section id="project-panel">
+            <ul>
+                <li>
+                    <h1>{title}</h1>
+                </li>
+                <li>
+                    <Button>Delete</Button>
+                </li>
+            </ul>
+
+            <p>{dueDate}</p>
+            <p>{desc}</p>
+
+            <hr />
+
+            <section id="project-panel">
+                <h2>Tasks</h2>
+                <div>
+                    <div>
+                        <Input />
+                        <Button>Add task</Button>
+                    </div>
+                    <ul>
+                        <li>
+                            <span>Learn the basics</span>
+                            <Button>Clear</Button>
+                        </li>
+                        <li>
+                            <span>Learn advanced concepts</span>
+                            <Button>Clear</Button>
+                        </li>
+                        <li>
+                            <span>Practice, practice, practice</span>
+                            <Button>Clear</Button>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </section>
     );
 }
