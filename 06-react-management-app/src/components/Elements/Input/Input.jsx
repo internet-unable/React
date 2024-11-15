501dc41f1e8b6ce54f91a716c60b303e7a7c3b85
internet-unable <k_moiseenko@icloud.com>
2024-11-15 12:38:21 +0200

    Update for Input component
    Styles for component

--- 06-react-management-app/src/components/Elements/Input/Input.jsx
+++ 06-react-management-app/src/components/Elements/Input/Input.jsx
@@ -1,12 +1,15 @@
 export default function Input({label, id, type = 'text'}) {
+    const labelStyles = 'text-neutral-500 font-semibold uppercase mb-1';
+    const inputStyles = 'bg-stone-300 px-2 py-2 rounded border-b-2 border-stone-400 focus:border-black';
+
     return(
         <>
-            <div className="flex flex-col">
-                {label && <label className="uppercase mb-1.5" for={id}>{label}</label>}
+            <div className="flex flex-col mb-4">
+                {label && <label className={labelStyles} for={id}>{label}</label>}
                 {type === 'textarea' ? (
-                    <textarea id={id} className=""></textarea>
+                    <textarea className={inputStyles} id={id}></textarea>
                 ) : (
-                    <input type={type} id={id} className="" />
+                    <input className={inputStyles} type={type} id={id} />
                 )}
             </div>
         </>
