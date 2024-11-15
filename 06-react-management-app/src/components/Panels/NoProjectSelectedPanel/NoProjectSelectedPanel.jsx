6f5d7e29a131052fbd20afdb980aea50c6ed4233
internet-unable <k_moiseenko@icloud.com>
2024-11-15 09:50:41 +0200

    Update fpr NoProjectSelectedPanel
    Styles update

--- 06-react-management-app/src/components/Panels/NoProjectSelectedPanel/NoProjectSelectedPanel.jsx
+++ 06-react-management-app/src/components/Panels/NoProjectSelectedPanel/NoProjectSelectedPanel.jsx
@@ -1,11 +1,12 @@
 import Button from "../../Elements/Button/Button.jsx";
+const imgPath = './public/logo.png';
 
 export default function NoProjectSelectedPanel({addProjectHandler}) {
     return(
-        <section className="h-screen">
-            <img src="" alt="" />
-            <h3>No Project Selected</h3>
-            <p>Select a project or get started with a new one</p>
+        <section className="h-screen flex flex-col justify-center items-center grow">
+            <img className="w-16 mb-4" src={imgPath} alt="" />
+            <h2 className="text-xl font-semibold mb-4">No Project Selected</h2>
+            <p className="mb-8">Select a project or get started with a new one</p>
             <Button onClick={addProjectHandler}>Create new project</Button>
         </section>
     );
