c9983d0b9cc8d650987566283521c285b8b0e30b
internet-unable <k_moiseenko@icloud.com>
2024-11-15 12:41:21 +0200

    Update for CreataProjectPanel component
    Add tailwind classes

--- 06-react-management-app/src/components/Panels/CreateProjectPanel/CreateProjectPanel.jsx
+++ 06-react-management-app/src/components/Panels/CreateProjectPanel/CreateProjectPanel.jsx
@@ -3,20 +3,20 @@ import Input from "../../Elements/Input/Input.jsx";
 
 export default function CreateProjectPanel() {
     return(
-        <section className="h-screen">
-            <ul>
+        <section className="h-screen flex flex-col items-center grow pt-24 pl-10 pr-40">
+            <ul className="w-full flex justify-end items-center mb-6">
                 <li>
-                    <Button btnType="text">Cancel</Button>
+                    <Button btnTypeText={true}>Cancel</Button>
                 </li>
                 <li>
                     <Button>Save</Button>
                 </li>
             </ul>
 
-            <form>
-                <Input label="Title" />
-                <Input label="Description" type="textarea" />
-                <Input label="Due date" type="date" />
+            <form className="w-full">
+                <Input id="title" label="Title" />
+                <Input id="desc "label="Description" type="textarea" />
+                <Input id="date "label="Due date" type="date" />
             </form>
         </section>
     );
