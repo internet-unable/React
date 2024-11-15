ac82c58b31500fd51e82db6a2402c67fd8bd2711
internet-unable <k_moiseenko@icloud.com>
2024-11-15 09:36:35 +0200

    Update for Button component
    Update styles and logic

--- 06-react-management-app/src/components/Elements/Button/Button.jsx
+++ 06-react-management-app/src/components/Elements/Button/Button.jsx
@@ -1,19 +1,27 @@
-export default function Button({btnType = 'button', children, onClick}) {
-    const typeButtonStyles = 'px-4 py-2 bg-black text-white rounded';
+export default function Button({btnTypeText, btnTypeOnDarkBg, children, onClick, ...props}) {
+    const onDarkBg = 'text-neutral-400 bg-stone-800 hover:text-neutral-300 hover:bg-stone-600';
+    const onLightBg = 'text-stone-50 bg-stone-900 hover:bg-stone-600';
+    const typeButtonStyles = 'px-5 py-2.5 rounded-md';
     const typeTextStyles = 'text-black';
-    let finalStyles = typeButtonStyles;
+    let finalStyles = `${typeButtonStyles} ${onLightBg}`;
 
-    if (btnType === 'text') {
+    if (btnTypeText) {
         finalStyles = typeTextStyles;
     }
 
+    if (btnTypeOnDarkBg) {
+        finalStyles = `${typeButtonStyles} ${onDarkBg}`;
+    }
+
     return(
-        <button
-            type="button"
-            className={finalStyles}
-            onClick={onClick}
-        >
-            {children}
-        </button>
+        <div {...props}>
+            <button
+                type="button"
+                className={finalStyles}
+                onClick={onClick}
+            >
+                {children}
+            </button>
+        </div>
     );
 }
