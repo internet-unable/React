ba957f2a58bdc5328e88f80d8b1d011d6d384d35
internet-unable <k_moiseenko@icloud.com>
2024-11-15 09:37:28 +0200

    Update SideMenu component
    Styles, structure and logic update

--- 06-react-management-app/src/components/SideMenu/SideMenu.jsx
+++ 06-react-management-app/src/components/SideMenu/SideMenu.jsx
@@ -1,14 +1,27 @@
 import Button from '../Elements/Button/Button.jsx';
 
-export default function SideMenu({addProjectHandler}) {
+export default function SideMenu({addProjectHandler, projectsList}) {
     return(
-        <aside className="h-screen">
-            <h2>Your projects</h2>
-            <Button onClick={addProjectHandler}>+ Add project</Button>
-            <ul>
-                <li>Project 1</li>
-                <li>Project 2</li>
-                <li>Project 3</li>
+        <aside className="flex flex-col h-screen w-80 bg-black rounded-tr-2xl px-10 py-20">
+            <div>
+                <h2 className="text-2xl font-semibold text-white uppercase mb-10">Your projects</h2>
+                <Button
+                    className="mb-10"
+                    onClick={addProjectHandler}
+                    btnTypeOnDarkBg={true}
+                >
+                    + Add project
+                </Button>
+            </div>
+            <ul className="overflow-auto">
+                {projectsList.map(item => (
+                    <li
+                        key={item.id}
+                        className="text-neutral-400 px-2 py-1 mb-1 cursor-pointer hover:text-neutral-300 hover:bg-stone-600"
+                    >
+                        {item.name}
+                    </li>
+                ))}
             </ul>
         </aside>
     );
