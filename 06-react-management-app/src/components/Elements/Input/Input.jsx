6f2c5323839ab63b3fd9aa6146b9c9606bdd47a2
internet-unable <k_moiseenko@icloud.com>
2024-11-18 16:30:13 +0200

    Update for Input component
    Validation styles code

--- 06-react-management-app/src/components/Elements/Input/Input.jsx
+++ 06-react-management-app/src/components/Elements/Input/Input.jsx
@@ -3,23 +3,25 @@ import { forwardRef } from "react";
 const BOTTOM_OUTLINE = 'bottom-outline';
 const ROUND_OUTLINE = 'rounded-outline';
 
-const Input = forwardRef(({label, id, type = 'text', stylesType, validateStyles, isInputValidStyles}, ref) => {
+const Input = forwardRef(({label, id, type = 'text', stylesType, isInputValid = true}, ref) => {
     const labelStyles = 'text-neutral-500 font-semibold uppercase mb-1';
     const defaultInputStyles = 'bg-stone-300 px-2 py-2 rounded';
-    const typeBottomOutlineInputStyles = 'border-b-2 border-stone-400 focus:border-black';
+    const typeBottomOutlineInputStyles = 'border-b-2';
     const typeRoundOutlineInputStyles = 'focus:outline focus:outline-2 focus:outline-blue-600';
     let finalInputStyles = '';
 
     if (stylesType === BOTTOM_OUTLINE) {
         finalInputStyles = `${defaultInputStyles} ${typeBottomOutlineInputStyles}`;
+
+        if (isInputValid) {
+            finalInputStyles += ' border-stone-400 focus:border-black';
+        } else {
+            finalInputStyles += ' border-red-500';
+        }
     }
 
     if (stylesType === ROUND_OUTLINE) {
         finalInputStyles = `${defaultInputStyles} ${typeRoundOutlineInputStyles}`
-    }
-
-    if (validateStyles && !isInputValidStyles) {
-        finalInputStyles += ' border-red-500';
     }
 
     return(
