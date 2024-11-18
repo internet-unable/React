b3e56ce25137a82e79bccde8b2e0a17c56ab6018
internet-unable <k_moiseenko@icloud.com>
2024-11-18 20:44:36 +0200

    Update for CreateProjectPanel component
    HTML structure update

--- 06-react-management-app/src/components/Panels/CreateProjectPanel/CreateProjectPanel.jsx
+++ 06-react-management-app/src/components/Panels/CreateProjectPanel/CreateProjectPanel.jsx
@@ -15,7 +15,7 @@ export default function CreateProjectPanel({cancelProjectCreationHandler, savePr
         } else {
             setIsProjectTitleValid(true);
             saveProjectHandler({
-                projectId: Math.random(),
+                projectId: `p${Math.random()}`,
                 projectTitle: projectTitleElem.current.value.trim(),
                 projectDesc: projectDescElem.current.value.trim(),
                 projectDate: projectDateElem.current.value.trim()
@@ -25,42 +25,47 @@ export default function CreateProjectPanel({cancelProjectCreationHandler, savePr
 
     return(
         <section className="h-screen flex flex-col items-center grow pt-24 pl-10 pr-40">
-            <ul className="w-full flex justify-end items-center mb-6">
-                <li>
-                    <Button
-                        btnTypeText={true}
-                        onClick={cancelProjectCreationHandler}
-                    >
-                        Cancel
-                    </Button>
-                </li>
-                <li>
-                    <Button onClick={handleValidation}>Save</Button>
-                </li>
-            </ul>
+            <form className="w-full max-w-screen-md">
+                <ul className="flex justify-end items-center mb-6 space-x-4">
+                    <li>
+                        <Button
+                            btnTypeText={true}
+                            onClick={cancelProjectCreationHandler}
+                        >
+                            Cancel
+                        </Button>
+                    </li>
+                    <li>
+                        <Button onClick={handleValidation}>Save</Button>
+                    </li>
+                </ul>
 
-            <form className="w-full">
-                <Input
-                    id="title"
-                    label="Title"
-                    ref={projectTitleElem}
-                    stylesType='bottom-outline'
-                    isInputValid={isProjectTitleValid}
-                />
-                <Input
-                    id="desc"
-                    label="Description"
-                    type="textarea"
-                    ref={projectDescElem}
-                    stylesType='bottom-outline'
-                />
-                <Input
-                    id="date"
-                    label="Due date"
-                    type="date"
-                    ref={projectDateElem}
-                    stylesType='bottom-outline'
-                />
+                <div className="space-y-4">
+                    <Input
+                        containerClasses="flex flex-col"
+                        label="Title"
+                        id="title"
+                        stylesType="bottom-outline"
+                        isInputValid={isProjectTitleValid}
+                        ref={projectTitleElem}
+                    />
+                    <Input
+                        containerClasses="flex flex-col"
+                        label="Description"
+                        id="desc"
+                        type="textarea"
+                        stylesType="bottom-outline"
+                        ref={projectDescElem}
+                    />
+                    <Input
+                        containerClasses="flex flex-col"
+                        label="Due date"
+                        id="date"
+                        type="date"
+                        stylesType="bottom-outline"
+                        ref={projectDateElem}
+                    />
+                </div>
             </form>
         </section>
     );
