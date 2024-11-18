867d89bcbe367718c8fdd7c67f45e276d57ac718
internet-unable <k_moiseenko@icloud.com>
2024-11-18 20:41:33 +0200

    Update dummy data file

--- 06-react-management-app/src/data.js
+++ 06-react-management-app/src/data.js
@@ -1,22 +1,102 @@
 export const PROJETS_LIST_BLUEPRINT = [
     {
-        projectId: 1,
-        projectTitle: "Dummy Project 1",
+        projectId: "p1",
+        projectTitle: "Dummy Project Title 1",
+        projectDesc: "Dummy Project Desc 1",
+        projectDate: "2024-12-01",
+        projectTasks: [
+            {
+                taskId: "p1-t1",
+                taskTitle: "Project 1 - Task 1",
+            },
+            {
+                taskId: "p1-t2",
+                taskTitle: "Project 1 - Task 2",
+            },
+            {
+                taskId: "p1-t3",
+                taskTitle: "Project 1 - Task 3",
+            }
+        ]
     },
     {
-        projectId: 2,
-        projectTitle: "Dummy Project 2",
+        projectId: "p2",
+        projectTitle: "Dummy Project Title 2",
+        projectDesc: "Dummy Project Desc 2",
+        projectDate: "2024-12-01",
+        projectTasks: [
+            {
+                taskId: "p2-t1",
+                taskTitle: "Project 2 - Task 1",
+            },
+            {
+                taskId: "p2-t2",
+                taskTitle: "Project 2 - Task 2",
+            },
+            {
+                taskId: "p2-t3",
+                taskTitle: "Project 2 - Task 3",
+            }
+        ]
     },
     {
-        projectId: 3,
-        projectTitle: "Dummy Project 3",
+        projectId: "p3",
+        projectTitle: "Dummy Project Title 3",
+        projectDesc: "Dummy Project Desc 3",
+        projectDate: "2024-12-01",
+        projectTasks: [
+            {
+                taskId: "p3-t1",
+                taskTitle: "Project 3 - Task 1",
+            },
+            {
+                taskId: "p3-t2",
+                taskTitle: "Project 3 - Task 2",
+            },
+            {
+                taskId: "p3-t3",
+                taskTitle: "Project 3 - Task 3",
+            }
+        ]
     },
     {
-        projectId: 4,
-        projectTitle: "Dummy Project 4",
+        projectId: "p4",
+        projectTitle: "Dummy Project Title 4",
+        projectDesc: "Dummy Project Desc 4",
+        projectDate: "2024-12-01",
+        projectTasks: [
+            {
+                taskId: "p4-t1",
+                taskTitle: "Project 4 - Task 1",
+            },
+            {
+                taskId: "p4-t2",
+                taskTitle: "Project 4 - Task 2",
+            },
+            {
+                taskId: "p4-t3",
+                taskTitle: "Project 4 - Task 3",
+            }
+        ]
     },
     {
-        projectId: 5,
-        projectTitle: "Dummy Project 5",
+        projectId: "p5",
+        projectTitle: "Dummy Project Title 5",
+        projectDesc: "Dummy Project Desc 5",
+        projectDate: "2024-12-01",
+        projectTasks: [
+            {
+                taskId: "p5-t1",
+                taskTitle: "Project 5 - Task 1",
+            },
+            {
+                taskId: "p5-t2",
+                taskTitle: "Project 5 - Task 2",
+            },
+            {
+                taskId: "p5-t3",
+                taskTitle: "Project 5 - Task 3",
+            }
+        ]
     }
 ];
