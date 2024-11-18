2f5cdf4d63afd073b48f7d66131fb8e45014f5c4
internet-unable <k_moiseenko@icloud.com>
2024-11-18 16:23:05 +0200

    Update for data.js
    Update structure of dummy data

--- 06-react-management-app/src/data.js
+++ 06-react-management-app/src/data.js
@@ -1,122 +1,22 @@
 export const PROJETS_LIST_BLUEPRINT = [
     {
-        id: 1,
-        name: "Project 1",
+        projectId: 1,
+        projectTitle: "Dummy Project 1",
     },
     {
-        id: 2,
-        name: "Project 2",
+        projectId: 2,
+        projectTitle: "Dummy Project 2",
     },
     {
-        id: 3,
-        name: "Project 3",
+        projectId: 3,
+        projectTitle: "Dummy Project 3",
     },
     {
-        id: 4,
-        name: "Project 4",
+        projectId: 4,
+        projectTitle: "Dummy Project 4",
     },
     {
-        id: 5,
-        name: "Project 5",
-    },
-    {
-        id: 6,
-        name: "Project 6",
-    },
-    {
-        id: 7,
-        name: "Project 7",
-    },
-    {
-        id: 8,
-        name: "Project 8",
-    },
-    {
-        id: 9,
-        name: "Project 9",
-    },
-    {
-        id: 10,
-        name: "Project 10",
-    },
-    {
-        id: 11,
-        name: "Project 11",
-    },
-    {
-        id: 12,
-        name: "Project 12",
-    },
-    {
-        id: 13,
-        name: "Project 13",
-    },
-    {
-        id: 14,
-        name: "Project 14",
-    },
-    {
-        id: 15,
-        name: "Project 15",
-    },
-    {
-        id: 16,
-        name: "Project 16",
-    },
-    {
-        id: 17,
-        name: "Project 17",
-    },
-    {
-        id: 18,
-        name: "Project 18",
-    },
-    {
-        id: 19,
-        name: "Project 19",
-    },
-    {
-        id: 20,
-        name: "Project 20",
-    },
-    {
-        id: 21,
-        name: "Project 21",
-    },
-    {
-        id: 22,
-        name: "Project 22",
-    },
-    {
-        id: 23,
-        name: "Project 23",
-    },
-    {
-        id: 24,
-        name: "Project 24",
-    },
-    {
-        id: 25,
-        name: "Project 25",
-    },
-    {
-        id: 26,
-        name: "Project 26",
-    },
-    {
-        id: 27,
-        name: "Project 27",
-    },
-    {
-        id: 28,
-        name: "Project 28",
-    },
-    {
-        id: 29,
-        name: "Project 29",
-    },
-    {
-        id: 30,
-        name: "Project 30",
+        projectId: 5,
+        projectTitle: "Dummy Project 5",
     }
 ];
