4b806b04d680ad3117a3a400ca307de3a96e9a24
internet-unable <k_moiseenko@icloud.com>
2024-11-18 20:39:07 +0200

    Update Button component
    Styles update for btn type text

--- 06-react-management-app/src/components/Elements/Button/Button.jsx
+++ 06-react-management-app/src/components/Elements/Button/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({btnTypeText, btnTypeOnDarkBg, children, onClick, ...props}) {
     const defaultStyles = 'px-5 py-2.5 rounded-md';
-    const typeTextStyles = `${defaultStyles} + text-black hover:text-red-500 focus:text-red-500`;
+    const typeTextStyles = 'text-black hover:text-red-500 focus:text-red-500 focus:outline-none focus:underline';
 
     const onDarkBg = 'text-neutral-400 bg-stone-800 hover:bg-stone-700 focus:bg-stone-700';
     const onLightBg = 'text-stone-50 bg-stone-900 hover:bg-stone-700 focus:bg-stone-700';
