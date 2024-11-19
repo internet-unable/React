3cff7227b7e666577ac14b30fbd2af4c229f1be5
internet-unable <k_moiseenko@icloud.com>
2024-11-19 20:17:01 +0200

    Update for NoProjectSelectedPanel component
    Styles update

--- 06-react-management-app/src/components/Panels/NoProjectSelectedPanel/NoProjectSelectedPanel.jsx
+++ 06-react-management-app/src/components/Panels/NoProjectSelectedPanel/NoProjectSelectedPanel.jsx
@@ -1,12 +1,12 @@
-import Button from "../../Elements/Button/Button.jsx";
-const imgPath = './public/logo.png';
+import Button from '../../Elements/Button/Button.jsx';
+import imgPath from '../../../assets/no-projects.png';
 
 export default function NoProjectSelectedPanel({addProjectHandler}) {
     return(
         <section className="h-full flex flex-col justify-center items-center grow">
-            <img className="w-16 mb-4" src={imgPath} alt="" />
-            <h2 className="text-xl font-semibold mb-4">No Project Selected</h2>
-            <p className="mb-8">Select a project or get started with a new one</p>
+            <img className="w-16 h-16 object-contain mb-4" src={imgPath} alt="" />
+            <h2 className="text-xl text-stone-500 font-bold mb-4">No Project Selected</h2>
+            <p className="text-syone-400 mb-8">Select a project or get started with a new one</p>
             <Button onClick={addProjectHandler}>Create new project</Button>
         </section>
     );
