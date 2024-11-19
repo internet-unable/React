fa0d5ee88d549ae32affba9c2d09659f3f6cea04
internet-unable <k_moiseenko@icloud.com>
2024-11-19 18:30:16 +0200

    Update Button component
    Combine styles

--- 06-react-management-app/src/components/Elements/Button/Button.jsx
+++ 06-react-management-app/src/components/Elements/Button/Button.jsx
@@ -1,9 +1,9 @@
 export default function Button({btnTypeText, btnTypeOnDarkBg, children, onClick, ...props}) {
-    const defaultStyles = 'px-4 py-2 rounded-md';
-    const typeTextStyles = 'text-black outline-none hover:text-red-500 focus-visible:text-red-500 focus-visible:underline';
+    const defaultStyles = 'px-4 py-2 rounded-md text-stone-400 outline-2 outline-offset-2 hover:text-stone-100 focus-visible:outline focus-visible:text-stone-100';
+    const typeTextStyles = 'text-stone-900 outline-none hover:text-red-500 focus-visible:text-red-500 focus-visible:underline';
 
-    const onDarkBg = 'text-stone-400 bg-stone-700 outline-2 outline-offset-2 outline-stone-100 hover:bg-stone-600 hover:text-stone-100 focus-visible:outline focus-visible:bg-stone-600 focus-visible:text-stone-100';
-    const onLightBg = 'text-stone-50 bg-stone-900 hover:bg-stone-700 outline-2 outline-offset-2 outline-stone-900 focus-visible:bg-stone-700 focus-visible:outline';
+    const onDarkBg = 'bg-stone-700 outline-stone-100 hover:bg-stone-600 focus-visible:bg-stone-600';
+    const onLightBg = 'bg-stone-900 outline-stone-900 hover:bg-stone-700 focus-visible:bg-stone-700';
 
     let finalStyles = `${defaultStyles} ${onLightBg}`;
 
