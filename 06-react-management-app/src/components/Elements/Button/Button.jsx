37c7e48491e239952b973e3b008707a5e32ddd4f
internet-unable <k_moiseenko@icloud.com>
2024-11-19 18:24:28 +0200

    Update Button component
    Update styles

--- 06-react-management-app/src/components/Elements/Button/Button.jsx
+++ 06-react-management-app/src/components/Elements/Button/Button.jsx
@@ -1,8 +1,8 @@
 export default function Button({btnTypeText, btnTypeOnDarkBg, children, onClick, ...props}) {
-    const defaultStyles = 'px-5 py-2.5 rounded-md';
+    const defaultStyles = 'px-4 py-2 rounded-md';
     const typeTextStyles = 'text-black outline-none hover:text-red-500 focus-visible:text-red-500 focus-visible:underline';
 
-    const onDarkBg = 'text-neutral-400 bg-stone-800 hover:bg-stone-700 outline-2 outline-offset-2 outline-neutral-400 focus-visible:bg-stone-700 focus-visible:outline';
+    const onDarkBg = 'text-stone-400 bg-stone-700 outline-2 outline-offset-2 outline-stone-100 hover:bg-stone-600 hover:text-stone-100 focus-visible:outline focus-visible:bg-stone-600 focus-visible:text-stone-100';
     const onLightBg = 'text-stone-50 bg-stone-900 hover:bg-stone-700 outline-2 outline-offset-2 outline-stone-900 focus-visible:bg-stone-700 focus-visible:outline';
 
     let finalStyles = `${defaultStyles} ${onLightBg}`;
