c22d6c0bcdf4320ff2804cf7475348cdc76cdece
internet-unable <k_moiseenko@icloud.com>
2024-11-20 10:51:34 +0200

    Update for CreateProjectPanel component
    Swithced from visual validation on input to showing Modal

--- 06-react-management-app/src/components/Panels/CreateProjectPanel/CreateProjectPanel.jsx
+++ 06-react-management-app/src/components/Panels/CreateProjectPanel/CreateProjectPanel.jsx
@@ -1,72 +1,85 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
+import Modal from "../../Modal/Modal.jsx";
 import Button from "../../Elements/Button/Button.jsx";
 import Input from "../../Elements/Input/Input.jsx";
 
 export default function CreateProjectPanel({cancelProjectCreationHandler, saveProjectHandler}) {
+    const modal = useRef();
     const projectTitleElem = useRef();
     const projectDescElem = useRef();
     const projectDateElem = useRef();
-    const [isProjectTitleValid, setIsProjectTitleValid] = useState(true);
 
     function handleValidation() {
-        if (!projectTitleElem.current.value.trim()) {
-            setIsProjectTitleValid(false);
+        const projectTitleElemValue = projectTitleElem.current.value.trim();
+        const projectDescElemValue = projectDescElem.current.value.trim();
+        const projectDateElemValue = projectDateElem.current.value.trim();
+
+        if (!projectTitleElemValue ||
+            !projectDescElemValue ||
+            !projectDateElemValue
+        ) {
+            modal.current.open();
         } else {
-            setIsProjectTitleValid(true);
             saveProjectHandler({
                 projectId: `p${Math.random()}`,
-                projectTitle: projectTitleElem.current.value.trim(),
-                projectDesc: projectDescElem.current.value.trim(),
-                projectDate: projectDateElem.current.value.trim()
+                projectTitle: projectTitleElemValue,
+                projectDesc: projectDescElemValue,
+                projectDate: projectDateElemValue
             });
         }
     }
 
     return(
-        <section className="h-full flex flex-col items-center grow py-16 pr-40 overflow-auto">
-            <div className="w-full max-w-screen-md">
-                <menu className="flex justify-end items-center mb-6 gap-4">
-                    <li>
-                        <Button
-                            isTypeText
-                            onClick={cancelProjectCreationHandler}
-                        >
-                            Cancel
-                        </Button>
-                    </li>
-                    <li>
-                        <Button onClick={handleValidation}>Save</Button>
-                    </li>
-                </menu>
+        <>
+            <Modal ref={modal}>
+                <h2 className="text-xl text-stone-700 font-bold mb-4">Invalid input</h2>
+                <p className="text-syone-600 mb-8">Please make sure that you provide a valid value for every input field.</p>
+            </Modal>
 
-                <div className="space-y-4">
-                    <Input
-                        containerClasses="flex flex-col"
-                        label="Title"
-                        id="title"
-                        stylesType="bottom-outline"
-                        isInputValid={isProjectTitleValid}
-                        ref={projectTitleElem}
-                    />
-                    <Input
-                        containerClasses="flex flex-col"
-                        label="Description"
-                        id="desc"
-                        type="textarea"
-                        stylesType="bottom-outline"
-                        ref={projectDescElem}
-                    />
-                    <Input
-                        containerClasses="flex flex-col"
-                        label="Due date"
-                        id="date"
-                        type="date"
-                        stylesType="bottom-outline"
-                        ref={projectDateElem}
-                    />
+            <section className="h-full flex flex-col items-center grow py-16 pr-40 overflow-auto">
+                <div className="w-full max-w-screen-md">
+                    <menu className="flex justify-end items-center mb-6 gap-4">
+                        <li>
+                            <Button
+                                isTypeText
+                                onClick={cancelProjectCreationHandler}
+                            >
+                                Cancel
+                            </Button>
+                        </li>
+                        <li>
+                            <Button onClick={handleValidation}>Save</Button>
+                        </li>
+                    </menu>
+
+                    <div className="space-y-4">
+                        <Input
+                            containerClasses="flex flex-col"
+                            label="Title"
+                            id="title"
+                            stylesType="bottom-outline"
+                            ref={projectTitleElem}
+                        />
+                        <Input
+                            containerClasses="flex flex-col"
+                            label="Description"
+                            id="desc"
+                            type="textarea"
+                            stylesType="bottom-outline"
+                            ref={projectDescElem}
+                        />
+                        <Input
+                            containerClasses="flex flex-col"
+                            label="Due date"
+                            id="date"
+                            type="date"
+                            stylesType="bottom-outline"
+                            ref={projectDateElem}
+                        />
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 }
