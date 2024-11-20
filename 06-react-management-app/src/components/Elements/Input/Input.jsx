232162799dc4ed30ee07bcf57fdcfd0e1d026590
internet-unable <k_moiseenko@icloud.com>
2024-11-20 14:03:22 +0200

    Update Input component
    Small styles update

--- 06-react-management-app/src/components/Elements/Input/Input.jsx
+++ 06-react-management-app/src/components/Elements/Input/Input.jsx
@@ -5,7 +5,7 @@ const ROUND_OUTLINE = 'rounded-outline';
 
 const Input = forwardRef(({containerClasses, label, id, type = 'text', stylesType}, ref) => {
     const labelStyles = 'text-sm font-bold uppercase text-stone-500 mb-1';
-    const defaultInputStyles = 'w-full p-1 rounded-sm text-stone-600 bg-stone-200 outline-none';
+    const defaultInputStyles = 'w-full p-2 rounded-sm text-stone-600 bg-stone-200 outline-none';
     const typeBottomOutlineInputStyles = 'border-b-2 border-stone-300 focus:border-stone-900';
     const typeRoundOutlineInputStyles = 'outline outline-2 outline-slate-50 focus:outline-blue-500';
     let finalInputStyles = '';
