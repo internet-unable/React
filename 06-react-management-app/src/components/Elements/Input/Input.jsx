405d21f60eb3a32d6ae99bb221ab04d36fa8080a
internet-unable <k_moiseenko@icloud.com>
2024-11-20 10:49:29 +0200

    Update Input component
    Delete condition styles that depends on validation

--- 06-react-management-app/src/components/Elements/Input/Input.jsx
+++ 06-react-management-app/src/components/Elements/Input/Input.jsx
@@ -3,31 +3,19 @@ import { forwardRef } from "react";
 const BOTTOM_OUTLINE = 'bottom-outline';
 const ROUND_OUTLINE = 'rounded-outline';
 
-const Input = forwardRef(({containerClasses, label, id, type = 'text', stylesType, isInputValid = true}, ref) => {
+const Input = forwardRef(({containerClasses, label, id, type = 'text', stylesType}, ref) => {
     const labelStyles = 'text-sm font-bold uppercase text-stone-500 mb-1';
     const defaultInputStyles = 'w-full p-1 rounded-sm text-stone-600 bg-stone-200 outline-none';
-    const typeBottomOutlineInputStyles = 'border-b-2';
-    const typeRoundOutlineInputStyles = 'outline outline-2';
+    const typeBottomOutlineInputStyles = 'border-b-2 border-stone-300 focus:border-stone-900';
+    const typeRoundOutlineInputStyles = 'outline outline-2 outline-slate-50 focus:outline-blue-500';
     let finalInputStyles = '';
 
     if (stylesType === BOTTOM_OUTLINE) {
         finalInputStyles = `${defaultInputStyles} ${typeBottomOutlineInputStyles}`;
-
-        if (isInputValid) {
-            finalInputStyles += ' border-stone-300 focus:border-stone-900';
-        } else {
-            finalInputStyles += ' border-red-500 focus:border-stone-900';
-        }
     }
 
     if (stylesType === ROUND_OUTLINE) {
         finalInputStyles = `${defaultInputStyles} ${typeRoundOutlineInputStyles}`
-
-        if (isInputValid) {
-            finalInputStyles += ' outline-slate-50 focus:outline-blue-500';
-        } else {
-            finalInputStyles += ' outline-red-500 focus:outline-blue-500';
-        }
     }
 
     return(
