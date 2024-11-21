ebbd87847a3643793346fbad1cefa44cdb3bd49d
internet-unable <k_moiseenko@icloud.com>
2024-11-21 17:53:58 +0200

    Update Products component
    Ger rid off prop drilling and use context instead

--- 07-react-shop/src/components/Product.jsx
+++ 07-react-shop/src/components/Product.jsx
@@ -1,24 +1,28 @@
+import { userContext } from 'react';
+import { CartContext } from "../store/shoping-cart-context";
+
 export default function Product({
-  id,
-  image,
-  title,
-  price,
-  description,
-  onAddToCart,
+    id,
+    image,
+    title,
+    price,
+    description
 }) {
-  return (
-    <article className="product">
-      <img src={image} alt={title} />
-      <div className="product-content">
-        <div>
-          <h3>{title}</h3>
-          <p className='product-price'>${price}</p>
-          <p>{description}</p>
-        </div>
-        <p className='product-actions'>
-          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
-        </p>
-      </div>
-    </article>
-  );
+    const { addItemToCart } = userContext(CartContext);
+
+    return (
+        <article className="product">
+            <img src={image} alt={title} />
+            <div className="product-content">
+                <div>
+                    <h3>{title}</h3>
+                    <p className='product-price'>${price}</p>
+                    <p>{description}</p>
+                </div>
+                <p className='product-actions'>
+                    <button onClick={() => addItemToCart(id)}>Add to Cart</button>
+                </p>
+            </div>
+        </article>
+    );
 }
