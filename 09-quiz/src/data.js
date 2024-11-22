a0d243218a317ae77ee9683800ebdae31a9e6a92
internet-unable <k_moiseenko@icloud.com>
2024-11-22 15:37:06 +0200

    Added dummy data blueprint

---
+++ 09-quiz/src/data.js
@@ -0,0 +1,106 @@
+export const QUESTIONS_BLUEPRINT = [
+    {
+        questionId: 'q1',
+        questionText: 'Question 1',
+        answers: [
+            {
+                answerId: 'q1-a1',
+                answerText: 'Question-1 - Answer-1',
+                isAnswerRight: true
+            },
+            {
+                answerId: 'q1-a2',
+                answerText: 'Question-1 - Answer-2',
+                isAnswerRight: false
+            },
+            {
+                answerId: 'q1-a3',
+                answerText: 'Question-1 - Answer-3',
+                isAnswerRight: false
+            },
+            {
+                answerId: 'q1-a4',
+                answerText: 'Question-1 - Answer-4',
+                isAnswerRight: false
+            }
+        ]
+    },
+    {
+        questionId: 'q2',
+        questionText: 'Question 2',
+        answers: [
+            {
+                answerId: 'q2-a1',
+                answerText: 'Question-2 - Answer-1',
+                isAnswerRight: false
+            },
+            {
+                answerId: 'q2-a2',
+                answerText: 'Question-2 - Answer-2',
+                isAnswerRight: true
+            },
+            {
+                answerId: 'q2-a3',
+                answerText: 'Question-2 - Answer-3',
+                isAnswerRight: false
+            },
+            {
+                answerId: 'q2-a4',
+                answerText: 'Question-2 - Answer-4',
+                isAnswerRight: false
+            }
+        ]
+    },
+    {
+        questionId: 'q3',
+        questionText: 'Question 3',
+        answers: [
+            {
+                answerId: 'q3-a1',
+                answerText: 'Question-3 - Answer-1',
+                isAnswerRight: false
+            },
+            {
+                answerId: 'q3-a2',
+                answerText: 'Question-3 - Answer-2',
+                isAnswerRight: false
+            },
+            {
+                answerId: 'q3-a3',
+                answerText: 'Question-3 - Answer-3',
+                isAnswerRight: true
+            },
+            {
+                answerId: 'q3-a4',
+                answerText: 'Question-3 - Answer-4',
+                isAnswerRight: false
+            }
+        ]
+    },
+    {
+        questionId: 'q4',
+        questionText: 'Question 4',
+        answers: [
+            {
+                answerId: 'q4-a1',
+                answerText: 'Question-4 - Answer-1',
+                isAnswerRight: false
+            },
+            {
+                answerId: 'q4-a2',
+                answerText: 'Question-4 - Answer-2',
+                isAnswerRight: false
+            },
+            {
+                answerId: 'q4-a3',
+                answerText: 'Question-4 - Answer-3',
+                isAnswerRight: false
+            },
+            {
+                answerId: 'q4-a4',
+                answerText: 'Question-4 - Answer-4',
+                isAnswerRight: true
+            }
+        ]
+    }
+];
