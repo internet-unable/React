2271dc878f0dff5be22cc34bf62a15836176c798
internet-unable <k_moiseenko@icloud.com>
2024-11-25 19:39:23 +0200

    Update App and QuestionPanel
    Prop drilling

--- 09-quiz/src/App.jsx
+++ 09-quiz/src/App.jsx
@@ -1,43 +1,20 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { QUESTIONS_BLUEPRINT } from './data.js';
 import QuestionPanel from './components/QuestionPanel/QuestionPanel.jsx';
 import SummaryPanel from './components/SummaryPanel/SummaryPanel.jsx';
-
-const TIMEOUT = 10000;
 
 function App() {
     const [userAnswers, setUserAnswers] = useState([]);
     const currentQuestionIndex = userAnswers.length;
     const isQuestionsAreOver = currentQuestionIndex === QUESTIONS_BLUEPRINT.length;
 
-    function handleAnswerSelect(answerId) {
+    const handleAnswerSelect = useCallback((answerId) => {
         setUserAnswers(prevState => {
             return [...prevState, answerId];
         });
-    }
+    }, []);
 
-    // const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    // const [userAnswers, setUserAnswers] = useState([]);
-    // const currentQuestionIndex = userAnswers.length;
-
-    // useEffect(() => {
-    //     let timeout;
-
-    //     if (currentQuestionIndex !== null) {
-    //         timeout = setTimeout(() => {
-    //             if (QUESTIONS_BLUEPRINT[currentQuestionIndex + 1]) {
-    //                 setCurrentQuestionIndex(currentQuestionIndex + 1);
-    //             } else {
-    //                 setCurrentQuestionIndex(null);
-    //                 clearTimeout(timeout);
-    //             }
-    //         }, TIMEOUT);
-    //     }
-
-    //     return () => {
-    //         clearTimeout(timeout);
-    //     }
-    // }, [currentQuestionIndex]);
+    const handleSkipAnswer = useCallback(() => handleAnswerSelect(null), [handleAnswerSelect]);
 
     return(
         <>
@@ -46,6 +23,7 @@ function App() {
                     <QuestionPanel
                         index={currentQuestionIndex}
                         answerSelectHandler={handleAnswerSelect}
+                        skipAnswerHandler={handleSkipAnswer}
                     />
                 </div>
             )}
--- 09-quiz/src/components/QuestionPanel/QuestionPanel.jsx
+++ 09-quiz/src/components/QuestionPanel/QuestionPanel.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import Progress from '../Progress/Progress.jsx';
 import Answer from '../Answer/Answer.jsx';
 import { QUESTIONS_BLUEPRINT } from '../../data.js';
 
-export default function QuestionPanel({ timeout, index, answerSelectHandler }) {
+const TIMEOUT = 10000;
+
+export default function QuestionPanel({ index, answerSelectHandler, skipAnswerHandler }) {
+    const handleTimeOut = useCallback(() => skipAnswerHandler());
+
     return (
         <div id="question">
-            <Progress timeout={timeout} />
+            <Progress
+                key={index}
+                timeOut={TIMEOUT}
+                timeOutHandler={handleTimeOut}
+            />
             <h2>{QUESTIONS_BLUEPRINT[index].questionText}</h2>
             <ul id="answers">
                 {QUESTIONS_BLUEPRINT[index].answers.map(answer => (
