59f792501d57e0825c9fe2e69141a43a19c3f66a
internet-unable <k_moiseenko@icloud.com>
2024-11-25 16:43:42 +0200

    Update Progress component
    Add interval update

--- 09-quiz/src/components/Progress/Progress.jsx
+++ 09-quiz/src/components/Progress/Progress.jsx
@@ -1,5 +1,27 @@
-export default function Progress() {
-    return(
-        <progress />
+import { useEffect, useState } from "react";
+
+const INTERVAL = 10;
+
+export default function Progress({ timeout }) {
+    const [progressValue, setProgressValue] = useState(timeout);
+
+    useEffect(() => {
+        let interval;
+
+        if (progressValue > 0) {
+            interval = setInterval(() => {
+                setProgressValue(prevState => {
+                    return prevState - INTERVAL;
+                })
+            }, INTERVAL);
+        }
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [progressValue]);
+
+    return (
+        <progress max={timeout} value={progressValue} />
     );
 }
