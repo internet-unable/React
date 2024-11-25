e03c68bb671d10cb1ddef6fb564b45fa3cc94708
internet-unable <k_moiseenko@icloud.com>
2024-11-25 15:45:57 +0200

    Update QuestionPanel component
    Output data from dummy data file

--- 09-quiz/src/components/QuestionPanel/QuestionPanel.jsx
+++ 09-quiz/src/components/QuestionPanel/QuestionPanel.jsx
@@ -1,21 +1,16 @@
-export default function QuestionPanel() {
-    return(
+import { QUESTIONS_BLUEPRINT } from '../../data.js';
+import Progress from '../Progress/Progress.jsx';
+import Answer from '../Answer/Answer.jsx';
+
+export default function QuestionPanel({ index }) {
+    return (
         <div id="question">
-            <progress />
-            <h2>Question title</h2>
+            <Progress />
+
+            <h2>{QUESTIONS_BLUEPRINT[index].questionText}</h2>
+
             <ul id="answers">
-                <li className="answer">
-                    <button>Answer 1</button>
-                </li>
-                <li className="answer">
-                    <button>Answer 1</button>
-                </li>
-                <li className="answer">
-                    <button>Answer 1</button>
-                </li>
-                <li className="answer">
-                    <button>Answer 1</button>
-                </li>
+                {QUESTIONS_BLUEPRINT[index].answers.map(answer => <Answer key={answer.answerId} text={answer.answerText} />)}
             </ul>
         </div>
     );
