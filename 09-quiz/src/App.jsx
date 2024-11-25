2b71793ae7eb4a98ef1a70ce76cde6964d1565a5
internet-unable <k_moiseenko@icloud.com>
2024-11-25 15:46:32 +0200

    Update App component
    Conditionaly show components

--- 09-quiz/src/App.jsx
+++ 09-quiz/src/App.jsx
@@ -1,14 +1,43 @@
+import { useEffect, useState } from "react";
+import { QUESTIONS_BLUEPRINT } from './data.js';
 import QuestionPanel from './components/QuestionPanel/QuestionPanel.jsx';
 import SummaryPanel from './components/SummaryPanel/SummaryPanel.jsx';
 
+const TIMEOUT = 10000;
+
 function App() {
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+    useEffect(() => {
+        let timeout;
+
+        if (currentQuestionIndex !== null) {
+            timeout = setTimeout(() => {
+                if (QUESTIONS_BLUEPRINT[currentQuestionIndex + 1]) {
+                    setCurrentQuestionIndex(currentQuestionIndex + 1);
+                } else {
+                    setCurrentQuestionIndex(null);
+                    clearTimeout(timeout);
+                }
+            }, TIMEOUT);
+        }
+
+        return () => {
+            clearTimeout(timeout);
+        }
+    }, [currentQuestionIndex]);
+
     return(
         <>
-            <div id="quiz">
-                <QuestionPanel />
-            </div>
+            {QUESTIONS_BLUEPRINT[currentQuestionIndex] && (
+                <div id="quiz">
+                    <QuestionPanel index={currentQuestionIndex} />
+                </div>
+            )}
 
-            <SummaryPanel />
+            {!QUESTIONS_BLUEPRINT[currentQuestionIndex] && (
+                <SummaryPanel />
+            )}
         </>
     );
 }
