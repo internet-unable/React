6a8d2645fd11504f9829b7fa531f006c56545842
internet-unable <k_moiseenko@icloud.com>
2024-11-29 20:41:07 +0200

    Update UserFinder
    Example of how to use context and Error Boundary approach

--- 10-class-based-project/src/components/UserFinder.js
+++ 10-class-based-project/src/components/UserFinder.js
@@ -1,15 +1,13 @@
 import { Component, Fragment } from "react";
 
+import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary.js"
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
 
-const DUMMY_USERS = [
-    { id: "u1", name: "Max" },
-    { id: "u2", name: "Manuel" },
-    { id: "u3", name: "Julie" },
-];
-
 class UserFinder extends Component {
+    static contextType = UsersContext;
+
     constructor() {
         super();
         this.state = {
@@ -18,19 +16,21 @@ class UserFinder extends Component {
         };
     }
 
+    componentDidCatch
+
     searchChangeHandler(event) {
         this.setState({ searchTerm: event.target.value });
     }
 
     componentDidMount() {
         // Send http request
-        this.setState({ filteredUsers: DUMMY_USERS });
+        this.setState({ filteredUsers: this.context.users });
     }
 
     componentDidUpdate(prevProps, prevState) {
         if (prevState.searchTerm !== this.state.searchTerm) {
             this.setState({
-                filteredUsers: DUMMY_USERS.filter((user) =>
+                filteredUsers: this.context.users.filter((user) =>
                     user.name.includes(this.state.searchTerm)
                 ),
             });
@@ -46,7 +46,9 @@ class UserFinder extends Component {
                         onChange={this.searchChangeHandler.bind(this)}
                     />
                 </div>
-                <Users users={this.state.filteredUsers} />
+                <ErrorBoundary>
+                    <Users users={this.state.filteredUsers} />
+                </ErrorBoundary>
             </Fragment>
         );
     }
