9b075db472f697f4ac45d20b1bca87d246548c7e
internet-unable <k_moiseenko@icloud.com>
2024-11-29 19:11:16 +0200

    Update User
    Example of how to use componentWillUnmount

--- 10-class-based-project/src/components/User.js
+++ 10-class-based-project/src/components/User.js
@@ -1,8 +1,11 @@
 import { Component } from "react";
 import classes from "./User.module.css";
 
-class User extends Component { // extends Component, makes posible to use this.props
-    // render() is a method that React expects for class component to have
+class User extends Component {
+    componentWillUnmount() {
+        console.log('User will unmount');
+    }
+    
     render() {
         return <li className={classes.user}>{this.props.name}</li>;
     }
