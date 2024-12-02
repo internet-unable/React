b7920496e57810c144d2e4ee1d3f3ec9e21e0294
internet-unable <k_moiseenko@icloud.com>
2024-12-02 19:15:50 +0200

    Update http.js
    Add PUT method

--- 11-react-http/src/http.js
+++ 11-react-http/src/http.js
@@ -2,9 +2,30 @@ export async function fetchAvailablePlaces() {
     const response = await fetch("http://localhost:3000/places");
     const resData = await response.json();
 
-    if (response.ok) { // status code 200/300
+    if (response.ok) {
+        // status code 200/300
         return resData.places;
-    } else { // status code 400/500
+    } else {
+        // status code 400/500
         throw new Error("Failed to fetch places");
+    }
+}
+
+export async function updateUserPlaces(places) {
+    const response = fetch("http://localhost:3000/user-places", {
+        method: "PUT",
+        body: JSON.stringify({places: places}),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    const resData = (await response).json();
+
+    if (response.ok) {
+        // status code 200/300
+        return resData.message;
+    } else {
+        // status code 400/500
+        throw new Error("Failed to update user data");
     }
 }
