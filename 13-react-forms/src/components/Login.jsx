9b52be3c93e249e913899a6d7de44a7e3714dc37
internet-unable <k_moiseenko@icloud.com>
2024-12-03 18:38:07 +0200

    Update Login
    Update component with posible approaches of form handling

--- 13-react-forms/src/components/Login.jsx
+++ 13-react-forms/src/components/Login.jsx
@@ -1,24 +1,78 @@
+import { useState } from "react";
+
 export default function Login() {
-  return (
-    <form>
-      <h2>Login</h2>
+    // const [emailInput, setEmailInput] = useState('');
+    // const [passwordInput, setPasswordInput] = useState('');
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
-        </div>
+    // function handleEmailInputChange(event) {
+    //     setEmailInput(event.target.value);
+    // }
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
-        </div>
-      </div>
+    // function handlePasswordInputChange(event) {
+    //     setPasswordInput(event.target.value);
+    // }
 
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
-      </p>
-    </form>
-  );
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        console.log(inputsValue);
+    }
+
+    const [inputsValue, setInputsValue] = useState({
+        email: '',
+        password: ''
+    });
+
+    function handleInputChange(inputType, value) {
+        setInputsValue(prevState => {
+            return {
+                ...prevState,
+                [inputType]: value
+            }
+        });
+    }
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <h2>Login</h2>
+
+            <div className="control-row">
+                <div className="control no-margin">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        name="email"
+                        // onChange={handleEmailInputChange}
+                        // value={emailInput}
+                        onChange={() => handleInputChange("email", event.target.value)}
+                        value={inputsValue.email}
+                    />
+                </div>
+
+                <div className="control no-margin">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        // onChange={handlePasswordInputChange}
+                        // value={passwordInput}
+                        onChange={() => handleInputChange("password", event.target.value)}
+                        value={inputsValue.password}
+                    />
+                </div>
+            </div>
+
+            <p className="form-actions">
+                <button type="button" className="button button-flat">Reset</button>
+                <button
+                    className="button"
+                    // type="button"
+                    // onClick={handleFormSubmit}
+                >
+                    Login
+                </button>
+            </p>
+        </form>
+    );
 }
