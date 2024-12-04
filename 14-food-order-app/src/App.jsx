9bb398dc05b5949f475d8b9a2381ba72f508722c
internet-unable <k_moiseenko@icloud.com>
2024-12-04 23:30:08 +0200

    Update App
    Update component, to make request to the server and render data

--- 14-food-order-app/src/App.jsx
+++ 14-food-order-app/src/App.jsx
@@ -1,15 +1,43 @@
+import { useEffect, useState } from 'react';
 import Modal from './components/Modal/Modal.jsx';
 import Cart from "./components/Cart/Cart.jsx";
 import Checkout from './components/Checkout/Checkout.jsx'
 import Product from './components/Product/Product.jsx';
 
 function App() {
+    const [isMealsFetching, setIsMealsFetching] = useState(false);
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        async function fetchMeals() {
+            setIsMealsFetching(true);
+            try {
+                const response = await fetch("http://localhost:3000/meals");
+                const meals = await response.json();
+
+                if (response.ok) {
+                    // status code 200/300
+                    setMeals(meals);
+                } else {
+                    // status code 400/500
+                    // throw new Error("Failed to fetch user places");
+                }
+
+            } catch (error) {
+                // setError({ message: message.error || 'Failed to feth user places' });
+            }
+            setIsMealsFetching(false);
+        };
+
+        fetchMeals();
+    }, []);
+
     return (
         <>
-            <Modal>
+            {/* <Modal>
                 <Cart />
-                {/* <Checkout /> */}
-            </Modal>
+                <Checkout />
+            </Modal> */}
 
             <header id="main-header">
                 <div id="title">
@@ -20,16 +48,13 @@ function App() {
             </header>
 
             <ul id="meals">
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
+                {isMealsFetching && <p>Meals are fetching</p>}
+
+                {!isMealsFetching && meals.map(meal => (
+                    <li className="meal-item" key={meal.id}>
+                        <Product props={meal} />
+                    </li>
+                ))}
             </ul>
         </>
     );
