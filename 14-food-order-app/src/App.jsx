23a2970184c0b76092d74fa00bd20b3e69943275
internet-unable <k_moiseenko@icloud.com>
2024-12-04 21:33:19 +0200

    Update App
    Update compoent with created components

--- 14-food-order-app/src/App.jsx
+++ 14-food-order-app/src/App.jsx
@@ -1,105 +1,35 @@
+import Modal from './components/Modal/Modal.jsx';
+import Cart from "./components/Cart/Cart.jsx";
+import Checkout from './components/Checkout/Checkout.jsx'
+import Product from './components/Product/Product.jsx';
+
 function App() {
     return (
         <>
+            <Modal>
+                <Cart />
+                {/* <Checkout /> */}
+            </Modal>
+
             <header id="main-header">
                 <div id="title">
                     <img />
                     <h1>Reactfood</h1>
                 </div>
-                <button type="button">Cart (3)</button>
+                <button type="button" className="button">Cart (3)</button>
             </header>
 
             <ul id="meals">
-                <li className="meal-item">
-                    <article>
-                        <img />
-                        <h3>Mac & Cheese</h3>
-                        <p className="meal-item-price">$8.99</p>
-                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
-                        <button type="button" className="meal-item-actions">Add to cart</button>
-                    </article>
-                </li>
-                <li className="meal-item">
-                    <article>
-                        <img />
-                        <h3>Mac & Cheese</h3>
-                        <p className="meal-item-price">$8.99</p>
-                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
-                        <button type="button" className="meal-item-actions">Add to cart</button>
-                    </article>
-                </li>
-                <li className="meal-item">
-                    <article>
-                        <img />
-                        <h3>Mac & Cheese</h3>
-                        <p className="meal-item-price">$8.99</p>
-                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
-                        <button type="button" className="meal-item-actions">Add to cart</button>
-                    </article>
-                </li>
-                <li className="meal-item">
-                    <article>
-                        <img />
-                        <h3>Mac & Cheese</h3>
-                        <p className="meal-item-price">$8.99</p>
-                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
-                        <button type="button" className="meal-item-actions">Add to cart</button>
-                    </article>
-                </li>
-                <li className="meal-item">
-                    <article>
-                        <img />
-                        <h3>Mac & Cheese</h3>
-                        <p className="meal-item-price">$8.99</p>
-                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
-                        <button type="button" className="meal-item-actions">Add to cart</button>
-                    </article>
-                </li>
-                <li className="meal-item">
-                    <article>
-                        <img />
-                        <h3>Mac & Cheese</h3>
-                        <p className="meal-item-price">$8.99</p>
-                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
-                        <button type="button" className="meal-item-actions">Add to cart</button>
-                    </article>
-                </li>
-                <li className="meal-item">
-                    <article>
-                        <img />
-                        <h3>Mac & Cheese</h3>
-                        <p className="meal-item-price">$8.99</p>
-                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
-                        <button type="button" className="meal-item-actions">Add to cart</button>
-                    </article>
-                </li>
-                <li className="meal-item">
-                    <article>
-                        <img />
-                        <h3>Mac & Cheese</h3>
-                        <p className="meal-item-price">$8.99</p>
-                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
-                        <button type="button" className="meal-item-actions">Add to cart</button>
-                    </article>
-                </li>
-                <li className="meal-item">
-                    <article>
-                        <img />
-                        <h3>Mac & Cheese</h3>
-                        <p className="meal-item-price">$8.99</p>
-                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
-                        <button type="button" className="meal-item-actions">Add to cart</button>
-                    </article>
-                </li>
-                <li className="meal-item">
-                    <article>
-                        <img />
-                        <h3>Mac & Cheese</h3>
-                        <p className="meal-item-price">$8.99</p>
-                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
-                        <button type="button" className="meal-item-actions">Add to cart</button>
-                    </article>
-                </li>
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
             </ul>
         </>
     );
