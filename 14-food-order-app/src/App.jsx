316589559cb3338a7e773b99ad0702b442e6ff7f
internet-unable <k_moiseenko@icloud.com>
2024-12-04 20:40:17 +0200

    Update App
    Create basic app structure

--- 14-food-order-app/src/App.jsx
+++ 14-food-order-app/src/App.jsx
@@ -1,11 +1,108 @@
 function App() {
-  return (
-    <>
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
-    </>
-  );
+    return (
+        <>
+            <header id="main-header">
+                <div id="title">
+                    <img />
+                    <h1>Reactfood</h1>
+                </div>
+                <button type="button">Cart (3)</button>
+            </header>
+
+            <ul id="meals">
+                <li className="meal-item">
+                    <article>
+                        <img />
+                        <h3>Mac & Cheese</h3>
+                        <p className="meal-item-price">$8.99</p>
+                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
+                        <button type="button" className="meal-item-actions">Add to cart</button>
+                    </article>
+                </li>
+                <li className="meal-item">
+                    <article>
+                        <img />
+                        <h3>Mac & Cheese</h3>
+                        <p className="meal-item-price">$8.99</p>
+                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
+                        <button type="button" className="meal-item-actions">Add to cart</button>
+                    </article>
+                </li>
+                <li className="meal-item">
+                    <article>
+                        <img />
+                        <h3>Mac & Cheese</h3>
+                        <p className="meal-item-price">$8.99</p>
+                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
+                        <button type="button" className="meal-item-actions">Add to cart</button>
+                    </article>
+                </li>
+                <li className="meal-item">
+                    <article>
+                        <img />
+                        <h3>Mac & Cheese</h3>
+                        <p className="meal-item-price">$8.99</p>
+                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
+                        <button type="button" className="meal-item-actions">Add to cart</button>
+                    </article>
+                </li>
+                <li className="meal-item">
+                    <article>
+                        <img />
+                        <h3>Mac & Cheese</h3>
+                        <p className="meal-item-price">$8.99</p>
+                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
+                        <button type="button" className="meal-item-actions">Add to cart</button>
+                    </article>
+                </li>
+                <li className="meal-item">
+                    <article>
+                        <img />
+                        <h3>Mac & Cheese</h3>
+                        <p className="meal-item-price">$8.99</p>
+                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
+                        <button type="button" className="meal-item-actions">Add to cart</button>
+                    </article>
+                </li>
+                <li className="meal-item">
+                    <article>
+                        <img />
+                        <h3>Mac & Cheese</h3>
+                        <p className="meal-item-price">$8.99</p>
+                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
+                        <button type="button" className="meal-item-actions">Add to cart</button>
+                    </article>
+                </li>
+                <li className="meal-item">
+                    <article>
+                        <img />
+                        <h3>Mac & Cheese</h3>
+                        <p className="meal-item-price">$8.99</p>
+                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
+                        <button type="button" className="meal-item-actions">Add to cart</button>
+                    </article>
+                </li>
+                <li className="meal-item">
+                    <article>
+                        <img />
+                        <h3>Mac & Cheese</h3>
+                        <p className="meal-item-price">$8.99</p>
+                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
+                        <button type="button" className="meal-item-actions">Add to cart</button>
+                    </article>
+                </li>
+                <li className="meal-item">
+                    <article>
+                        <img />
+                        <h3>Mac & Cheese</h3>
+                        <p className="meal-item-price">$8.99</p>
+                        <p className="meal-item-description">Creamy cheddar cheese mixed with perfeclty cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.</p>
+                        <button type="button" className="meal-item-actions">Add to cart</button>
+                    </article>
+                </li>
+            </ul>
+        </>
+    );
 }
 
 export default App;
