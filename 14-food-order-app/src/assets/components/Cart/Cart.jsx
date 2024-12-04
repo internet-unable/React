4076bd3b6690925e12d007a1650fa5a60455ad51
internet-unable <k_moiseenko@icloud.com>
2024-12-04 21:10:42 +0200

    Update Cart
    Update component with basic structure

--- 14-food-order-app/src/assets/components/Cart/Cart.jsx
+++ 14-food-order-app/src/assets/components/Cart/Cart.jsx
@@ -1,7 +1,40 @@
 export default function Cart() {
-    return(
-        <>
-            Cart
-        </>
+    return (
+        <div className="cart">
+            <h2>Your cart</h2>
+
+            <ul>
+                <li className="cart-item">
+                    <p>Seafood Paella</p>
+                    <div className="cart-item-actions">
+                        <button type="button">-</button>
+                        1
+                        <button type="button">+</button>
+                    </div>
+                </li>
+                <li className="cart-item">
+                    <p>Sushi Roll Platter</p>
+                    <div className="cart-item-actions">
+                        <button type="button">-</button>
+                        1
+                        <button type="button">+</button>
+                    </div>
+                </li>
+                <li className="cart-item">
+                    <p>Steak Frites</p>
+                    <div className="cart-item-actions">
+                        <button type="button">-</button>
+                        1
+                        <button type="button">+</button>
+                    </div>
+                </li>
+            </ul>
+
+            <div className="cart-total">$53.97</div>
+            <div className="modal-actions">
+                <button type="button" className="text-button">Close</button>
+                <button type="button" className="button">Go to Checkout</button>
+            </div>
+        </div>
     );
 }
