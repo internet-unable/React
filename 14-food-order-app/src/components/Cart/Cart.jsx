3fc7791cdc076ca77716e12464cd7463ae2205c2
internet-unable <k_moiseenko@icloud.com>
2024-12-05 21:27:06 +0200

    Update Cart
    Update component to work with context

--- 14-food-order-app/src/components/Cart/Cart.jsx
+++ 14-food-order-app/src/components/Cart/Cart.jsx
@@ -1,14 +1,12 @@
-import { useState, useEffect } from "react";
+import { useContext } from "react";
+import { AppContext } from '../../store/cart-context.jsx';
 
-export default function Cart({ cart }) {
-    const [modifiedCart, setModifiedCart] = useState(() => countDuplicates(cart));
-    const total = countTotal(modifiedCart).toFixed(2);
+export default function Cart() {
+    const { cart, updateCart } = useContext(AppContext);
+    const combinedCart = combineDuplicates(cart);
+    const totalSum = countTotalSum(cart).toFixed(2);
 
-    useEffect(() => {
-        setModifiedCart(countDuplicates(cart));
-    }, [cart]);
-
-    function countDuplicates(array) {
+    function combineDuplicates(array) {
         return Object.values(
             array.reduce((acc, item) => {
                 if (acc[item.id]) {
@@ -22,7 +20,7 @@ export default function Cart({ cart }) {
         );
     }
 
-    function countTotal(array) {
+    function countTotalSum(array) {
         return array.reduce((acc, item) => {
             if (item.count) {
                 acc += Number(item.price) * item.count;
@@ -34,43 +32,26 @@ export default function Cart({ cart }) {
         }, 0);
     }
 
-    function handleCountUpdate(isIncrement, id) {
-        setModifiedCart(prevState => {
-            return prevState.reduce((acc, meal) => {
-                if (meal.id === id) {
-                    const newCount = isIncrement ? meal.count + 1 : meal.count - 1;
-
-                    if (newCount > 0) {
-                        acc.push({ ...meal, count: newCount });
-                    }
-                } else {
-                    acc.push(meal);
-                }
-                return acc;
-            }, []);
-        });
-    }
-
     return (
         <div className="cart">
             <h2>Your cart</h2>
 
-            {modifiedCart.length > 0 && (
+            {combinedCart.length > 0 && (
                 <ul>
-                    {modifiedCart.map(meal => (
+                    {combinedCart.map(meal => (
                         <li className="cart-item" key={meal.id}>
                             <p>{meal.name} - {meal.count} x ${meal.price}</p>
                             <div className="cart-item-actions">
-                                <button type="button" onClick={() => handleCountUpdate(false, meal.id)}>-</button>
+                                <button type="button" onClick={() => updateCart(false, meal.id)}>-</button>
                                 {meal.count}
-                                <button type="button" onClick={() => handleCountUpdate(true, meal.id)}>+</button>
+                                <button type="button" onClick={() => updateCart(true, meal.id)}>+</button>
                             </div>
                         </li>
                     ))}
                 </ul>
             )}
 
-            <div className="cart-total">${total}</div>
+            <div className="cart-total">${totalSum}</div>
 
             <div className="modal-actions">
                 <button type="button" className="text-button">Close</button>
