d62f33a655e4f8e4b8bbdabd2f95c8002ded6a04
internet-unable <k_moiseenko@icloud.com>
2024-12-05 17:03:12 +0200

    Update Cart
    Handle cart on data update

--- 14-food-order-app/src/components/Cart/Cart.jsx
+++ 14-food-order-app/src/components/Cart/Cart.jsx
@@ -1,36 +1,77 @@
-export default function Cart() {
+import { useState, useEffect } from "react";
+
+export default function Cart({ cart }) {
+    const [modifiedCart, setModifiedCart] = useState(() => countDuplicates(cart));
+    const total = countTotal(modifiedCart).toFixed(2);
+
+    useEffect(() => {
+        setModifiedCart(countDuplicates(cart));
+    }, [cart]);
+
+    function countDuplicates(array) {
+        return Object.values(
+            array.reduce((acc, item) => {
+                if (acc[item.id]) {
+                    acc[item.id].count += 1;
+                } else {
+                    acc[item.id] = { ...item, count: 1 };
+                }
+
+                return acc;
+            }, {})
+        );
+    }
+
+    function countTotal(array) {
+        return array.reduce((acc, item) => {
+            if (item.count) {
+                acc += Number(item.price) * item.count;
+            } else {
+                acc += Number(item.price);
+            }
+
+            return acc;
+        }, 0);
+    }
+
+    function handleCountUpdate(isIncrement, id) {
+        setModifiedCart(prevState => {
+            return prevState.reduce((acc, meal) => {
+                if (meal.id === id) {
+                    const newCount = isIncrement ? meal.count + 1 : meal.count - 1;
+
+                    if (newCount > 0) {
+                        acc.push({ ...meal, count: newCount });
+                    }
+                } else {
+                    acc.push(meal);
+                }
+                return acc;
+            }, []);
+        });
+    }
+
     return (
         <div className="cart">
             <h2>Your cart</h2>
 
-            <ul>
-                <li className="cart-item">
-                    <p>Seafood Paella</p>
-                    <div className="cart-item-actions">
-                        <button type="button">-</button>
-                        1
-                        <button type="button">+</button>
-                    </div>
-                </li>
-                <li className="cart-item">
-                    <p>Sushi Roll Platter</p>
-                    <div className="cart-item-actions">
-                        <button type="button">-</button>
-                        1
-                        <button type="button">+</button>
-                    </div>
-                </li>
-                <li className="cart-item">
-                    <p>Steak Frites</p>
-                    <div className="cart-item-actions">
-                        <button type="button">-</button>
-                        1
-                        <button type="button">+</button>
-                    </div>
-                </li>
-            </ul>
+            {modifiedCart.length > 0 && (
+                <ul>
+                    {modifiedCart.map(meal => (
+                        <li className="cart-item" key={meal.id}>
+                            <p>{meal.name} - {meal.count} x ${meal.price}</p>
+                            <div className="cart-item-actions">
+                                <button type="button" onClick={() => handleCountUpdate(false, meal.id)}>-</button>
+                                {meal.count}
+                                <button type="button" onClick={() => handleCountUpdate(true, meal.id)}>+</button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            )}
 
-            <div className="cart-total">$53.97</div>
+            <div className="cart-total">${total}</div>
+
             <div className="modal-actions">
                 <button type="button" className="text-button">Close</button>
                 <button type="button" className="button">Go to Checkout</button>
