e5587e396e8c837d6794bfb884cee3e35e73716f
internet-unable <k_moiseenko@icloud.com>
2024-12-05 17:01:46 +0200

    Update Product
    Prop name change and add handler that add meal to cart

--- 14-food-order-app/src/components/Product/Product.jsx
+++ 14-food-order-app/src/components/Product/Product.jsx
@@ -1,14 +1,27 @@
-export default function Product({ props }) {
+export default function Product({ product, onAddMealToCart }) {
+    function handleAddToCartClick() {
+        onAddMealToCart({
+            id: product.id,
+            name: product.name,
+            price: product.price
+        });
+    }
     return (
         <article>
-            <img src={`http://localhost:3000/${props.image}`} alt={props.description} />
-            <h3>{props.name}</h3>
+            <img src={`http://localhost:3000/${product.image}`} alt={product.description} />
+            <h3>{product.name}</h3>
             <div>
-                <p className="meal-item-price">${props.price}</p>
+                <p className="meal-item-price">${product.price}</p>
             </div>
-            <p className="meal-item-description">{props.description}</p>
+            <p className="meal-item-description">{product.description}</p>
             <div>
-                <button type="button" className="button meal-item-actions">Add to cart</button>
+                <button
+                    type="button"
+                    className="button meal-item-actions"
+                    onClick={handleAddToCartClick}
+                >
+                    Add to cart
+                </button>
             </div>
         </article>
     );
