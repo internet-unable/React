4f89329873b7f9dd23e07a01aca0a91b8c27b6f3
internet-unable <k_moiseenko@icloud.com>
2024-12-06 15:34:10 +0200

    Update Cart
    Update component structure. Move totalSum to context

--- 14-food-order-app/src/components/Cart/Cart.jsx
+++ 14-food-order-app/src/components/Cart/Cart.jsx
@@ -2,9 +2,8 @@ import { useContext } from "react";
 import { AppContext } from '../../store/cart-context.jsx';
 
 export default function Cart() {
-    const { cart, updateCart } = useContext(AppContext);
+    const { cart, cartTotalSum, updateCart } = useContext(AppContext);
     const combinedCart = combineDuplicates(cart);
-    const totalSum = countTotalSum(cart).toFixed(2);
 
     function combineDuplicates(array) {
         return Object.values(
@@ -20,43 +19,29 @@ export default function Cart() {
         );
     }
 
-    function countTotalSum(array) {
-        return array.reduce((acc, item) => {
-            if (item.count) {
-                acc += Number(item.price) * item.count;
-            } else {
-                acc += Number(item.price);
-            }
-
-            return acc;
-        }, 0);
-    }
-
     return (
         <div className="cart">
-            <h2>Your cart</h2>
+            {!combinedCart.length && <h2>Your cart is empty</h2>}
 
             {combinedCart.length > 0 && (
-                <ul>
-                    {combinedCart.map(meal => (
-                        <li className="cart-item" key={meal.id}>
-                            <p>{meal.name} - {meal.count} x ${meal.price}</p>
-                            <div className="cart-item-actions">
-                                <button type="button" onClick={() => updateCart(false, meal.id)}>-</button>
-                                {meal.count}
-                                <button type="button" onClick={() => updateCart(true, meal.id)}>+</button>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+                <>
+                    <h2>Your cart</h2>
+
+                    <ul>
+                        {combinedCart.map(meal => (
+                            <li className="cart-item" key={meal.id}>
+                                <p>{meal.name} - {meal.count} x ${meal.price}</p>
+                                <div className="cart-item-actions">
+                                    <button type="button" onClick={() => updateCart(false, meal.id)}>-</button>
+                                    {meal.count}
+                                    <button type="button" onClick={() => updateCart(true, meal.id)}>+</button>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="cart-total">${cartTotalSum}</div>
+                </>
             )}
-
-            <div className="cart-total">${totalSum}</div>
-
-            <div className="modal-actions">
-                <button type="button" className="text-button">Close</button>
-                <button type="button" className="button">Go to Checkout</button>
-            </div>
         </div>
     );
 }
