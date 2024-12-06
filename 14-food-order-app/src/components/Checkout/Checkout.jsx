e5eeebd824b087064c96e2e5792408470aa19eb1
internet-unable <k_moiseenko@icloud.com>
2024-12-06 15:33:28 +0200

    Update Checkout
    Update component structure and add context, to use cartTotalSum

--- 14-food-order-app/src/components/Checkout/Checkout.jsx
+++ 14-food-order-app/src/components/Checkout/Checkout.jsx
@@ -1,41 +1,41 @@
+import { useContext } from "react";
+import { AppContext } from '../../store/cart-context.jsx';
+
 export default function Checkout() {
+    const { cartTotalSum } = useContext(AppContext);
+
     return (
         <>
             <h2>Checkout</h2>
-            <p>Total amount: $89.95</p>
+            <p>Total amount: ${cartTotalSum}</p>
 
-            <form className="control">
-                <div>
+            <form>
+                <div className="control">
                     <label>Full name</label>
                     <input type="text" />
                 </div>
 
-                <div>
+                <div className="control">
                     <label>E-mail</label>
                     <input type="email" />
                 </div>
 
-                <div>
+                <div className="control">
                     <label>Street</label>
                     <input type="text" />
                 </div>
 
                 <div className="control-row">
-                    <div>
+                    <div className="control">
                         <label>Postal Code</label>
                         <input type="text" />
                     </div>
-                    <div>
+                    <div className="control">
                         <label>City</label>
                         <input type="text" />
                     </div>
                 </div>
             </form>
-
-            <div className="modal-actions">
-                <button type="button" className="text-button">Close</button>
-                <button type="button" className="button">Submit order</button>
-            </div>
         </>
     );
 }
