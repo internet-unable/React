7efa467aa66d9a66093bed4a183c1886296e5c08
internet-unable <k_moiseenko@icloud.com>
2024-12-06 15:38:46 +0200

    Update Modal
    Update component with context, useImperativeHandle and forwardRef

--- 14-food-order-app/src/components/Modal/Modal.jsx
+++ 14-food-order-app/src/components/Modal/Modal.jsx
@@ -1,7 +1,48 @@
-export default function Modal({ children }) {
-    return (
-        <dialog className="modal" open>
+import { forwardRef, useRef, useContext, useImperativeHandle } from 'react';
+import { AppContext } from '../../store/cart-context.jsx';
+import { createPortal } from 'react-dom';
+
+const Modal = forwardRef(({ children, onCloseDialogClick, nextStepLable, onNextStepClick }, ref) => {
+    const { cart } = useContext(AppContext);
+    const dialog = useRef();
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            },
+            close() {
+                dialog.current.close();
+            }
+        };
+    });
+
+    return createPortal(
+        <dialog className="modal" ref={dialog}>
             {children}
-        </dialog>
+            
+            <div className="modal-actions">
+                <button
+                    type="button"
+                    className="text-button"
+                    onClick={onCloseDialogClick}
+                >
+                    Close
+                </button>
+
+                {cart.length > 0 && (
+                    <button
+                        type="button"
+                        className="button"
+                        onClick={onNextStepClick}
+                    >
+                        {nextStepLable}
+                    </button>
+                )}
+            </div>
+        </dialog>,
+        document.getElementById('modal')
     );
-}
+});
+
+export default Modal;
