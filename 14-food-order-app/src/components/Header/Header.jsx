fe5c005a0892da833df6b8d4fbe32276ea60c5a5
internet-unable <k_moiseenko@icloud.com>
2024-12-06 17:32:42 +0200

    Update header
    Add logo

--- 14-food-order-app/src/components/Header/Header.jsx
+++ 14-food-order-app/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { AppContext } from '../../store/cart-context.jsx';
+import logoPath from '../../assets/logo.jpg';
 
 export default function Header({ onCartClick }) {
     const { cart } = useContext(AppContext);
@@ -7,15 +8,11 @@ export default function Header({ onCartClick }) {
     return (
         <header id="main-header">
             <div id="title">
-                <img />
+                <img src={logoPath} alt="" />
                 <h1>Reactfood</h1>
             </div>
 
-            <button
-                type="button"
-                className="text-button"
-                onClick={onCartClick}
-            >
+            <button type="button" className="text-button" onClick={onCartClick}>
                 Cart {cart.length > 0 && `(${cart.length})`}
             </button>
         </header>
