58745a78a36b2e277ac5fa4c73640ce90aa4743b
internet-unable <k_moiseenko@icloud.com>
2024-12-11 22:01:17 +0200

    Update store
    Change from if statement to switch. Add extra action into reducer

--- 15-redux-demo-app/src/store/index.js
+++ 15-redux-demo-app/src/store/index.js
@@ -1,19 +1,37 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + action.amount
-        };
-    }
+const initialState = {
+    counter: 0,
+    showCounter: true
+};
 
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - action.amount
-        };
-    }
+const counterReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'increment':
+            return {
+                ...state,
+                counter: state.counter + action.amount
+            };
+            break;
 
-    return state;
+        case 'decrement': 
+            return {
+                ...state,
+                counter: state.counter - action.amount
+            };
+            break;
+
+        case 'toogle':
+            return {
+                ...state,
+                showCounter: !state.showCounter
+            }
+            break;
+
+        default:
+            return state;
+            break;
+    }
 };
 
 const store = createStore(counterReducer);
