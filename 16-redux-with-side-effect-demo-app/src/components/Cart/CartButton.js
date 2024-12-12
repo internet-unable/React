1bb6af453095358a7aac1ea058c0f1bc0ea93aea
internet-unable <k_moiseenko@icloud.com>
2024-12-12 20:04:39 +0200

    Update CartButton
    Apply store actions to component

--- 16-redux-with-side-effect-demo-app/src/components/Cart/CartButton.js
+++ 16-redux-with-side-effect-demo-app/src/components/Cart/CartButton.js
@@ -1,12 +1,20 @@
-import classes from './CartButton.module.css';
+import { useDispatch }  from "react-redux";
+import { uiActions } from "../../store/ui-slice.js";
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+    const dispatch = useDispatch();
+
+    const handleCartToogle = () => {
+        dispatch(uiActions.toogle());
+    }
+
+    return (
+        <button className={classes.button} onClick={handleCartToogle}>
+            <span>My Cart</span>
+            <span className={classes.badge}>1</span>
+        </button>
+    );
 };
 
 export default CartButton;
