4a5948c684c7f6548954ec63dbcba15c2334e8fd
internet-unable <k_moiseenko@icloud.com>
2024-12-17 11:50:43 +0200

    Update Cart

--- 16-redux-with-side-effect-demo-app/src/components/Cart/CartItem.js
+++ 16-redux-with-side-effect-demo-app/src/components/Cart/CartItem.js
@@ -1,28 +1,46 @@
-import classes from './CartItem.module.css';
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
+import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+    const { id, name, quantity, totalPrice, price } = props.item;
+    const dispatch = useDispatch();
 
-  return (
-    <li className={classes.item}>
-      <header>
-        <h3>{title}</h3>
-        <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
-          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
-        </div>
-      </header>
-      <div className={classes.details}>
-        <div className={classes.quantity}>
-          x <span>{quantity}</span>
-        </div>
-        <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
-        </div>
-      </div>
-    </li>
-  );
+    const handleAddItem = () => {
+        dispatch(cartActions.addItem({
+            id,
+            price,
+            quantity,
+            totalPrice
+        }));
+    }
+
+    const handleRemoveItem = () => {
+        dispatch(cartActions.removeItem(id));
+    }
+
+    return (
+        <li className={classes.item}>
+            <header>
+                <h3>{name}</h3>
+                <div className={classes.price}>
+                    ${totalPrice.toFixed(2)}{" "}
+                    <span className={classes.itemprice}>
+                        (${price.toFixed(2)}/item)
+                    </span>
+                </div>
+            </header>
+            <div className={classes.details}>
+                <div className={classes.quantity}>
+                    x <span>{quantity}</span>
+                </div>
+                <div className={classes.actions}>
+                    <button onClick={handleRemoveItem}>-</button>
+                    <button onClick={handleAddItem}>+</button>
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default CartItem;
--- 16-redux-with-side-effect-demo-app/src/store/cart-slice.js
+++ 16-redux-with-side-effect-demo-app/src/store/cart-slice.js
@@ -4,12 +4,14 @@ const cartSlice = createSlice({
     name: "cart",
     initialState: {
         items: [],
-        totalQuantity: 0
+        totalQuantity: 0,
     },
     reducers: {
         addItem(state, action) {
             const newItem = action.payload;
-            const itemInCart = state.items.find(item => item.id === newItem.id);
+            const itemInCart = state.items.find(
+                (item) => item.id === newItem.id
+            );
             state.totalQuantity += 1;
 
             if (itemInCart) {
@@ -20,22 +22,27 @@ const cartSlice = createSlice({
                     id: newItem.id,
                     price: newItem.price,
                     quantity: 1,
-                    totalPrice: newItem.price
+                    totalPrice: newItem.price,
                 });
             }
         },
         removeItem(state, action) {
             const id = action.payload;
-            const itemInCart = state.items.find(item => item.id === id);
+            const itemInCart = state.items.find((item) => item.id === id);
             state.totalQuantity -= 1;
 
+            console.log(state.isCartVisible);
             if (itemInCart.quantity === 1) {
-                state.items = state.items.filter(item => item.id !== id);
+                state.items = state.items.filter((item) => item.id !== id);
             } else {
                 itemInCart.quantity -= 1;
                 itemInCart.totalPrice -= itemInCart.price;
             }
-        }
+        },
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+        },
     },
 });
 
