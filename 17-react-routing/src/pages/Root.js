700de4418acb534a445def4e973840edd3ff93ef
internet-unable <k_moiseenko@icloud.com>
2024-12-18 12:35:09 +0200

    Update Root
    Move styles to index.css

--- 17-react-routing/src/pages/Root.js
+++ 17-react-routing/src/pages/Root.js
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 
-import classes from './Root.module.css';
-import MainNavigation from "../../components/MainNavigation";
+import MainNavigation from "../components/MainNavigation";
 
 export default function RootLayout() {
     return (
         <>
             <MainNavigation />
-            <main className={classes.content}>
+            <main>
                 <Outlet />
             </main>
         </>
