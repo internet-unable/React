62909053a567b075c3191d656d57847f3e6b9314
internet-unable <k_moiseenko@icloud.com>
2024-12-18 15:14:34 +0200

    Update Products
    Render dummy products

--- 17-react-routing/src/pages/Products.js
+++ 17-react-routing/src/pages/Products.js
@@ -1,10 +1,27 @@
 import { Link } from "react-router-dom";
 
+const DUMMY_PRODUCTS = [
+    { id: 1, title: "Product 1" },
+    { id: 2, title: "Product 2" },
+    { id: 3, title: "Product 3" },
+];
+
 export default function ProductsPage() {
     return (
         <>
             <h1>Products page</h1>
-            <p>Go to <Link to="/">Home page</Link></p>
+            <p>
+                Go to <Link to="/">Home page</Link>
+            </p>
+            <ul>
+                {DUMMY_PRODUCTS.map((product) => (
+                    <li>
+                        <Link to={`/products/product-${product.id}`}>
+                            {product.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </>
     );
 }
