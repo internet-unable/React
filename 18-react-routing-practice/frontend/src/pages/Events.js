75958b448e312115879df7b602a07988ab1d6ef8
internet-unable <k_moiseenko@icloud.com>
2024-12-18 17:20:43 +0200

    Update Event
    Output dummy data, with NavLink

--- 18-react-routing-practice/frontend/src/pages/Events.js
+++ 18-react-routing-practice/frontend/src/pages/Events.js
@@ -1,5 +1,22 @@
+import { NavLink } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+    { id: 1, title: "Event 1" },
+    { id: 2, title: "Event 2" },
+    { id: 3, title: "Event 3" },
+];
+
 export default function EventsPage() {
-    return(
-        <h1>Events page</h1>
+    return (
+        <>
+            <h1>Events page</h1>
+            <ul>
+                {DUMMY_EVENTS.map(event => (
+                    <li key={event.id}>
+                        <NavLink to={`/events/${event.id}`}>{event.title}</NavLink>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
