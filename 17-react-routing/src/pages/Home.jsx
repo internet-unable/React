8df3d20bd533f8e72f7835b3757cdd02a4a8ae23
internet-unable <k_moiseenko@icloud.com>
2024-12-18 11:48:27 +0200

    Update Home and Products
    Add navigation link

--- 17-react-routing/src/pages/Home.jsx
+++ 17-react-routing/src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
     return (
-        <h1>Home page</h1>
+        <>
+            <h1>Home page</h1>
+            <p>Go to <Link to="/products">Products page</Link></p>
+        </>
     );
 }
--- 17-react-routing/src/pages/Products.jsx
+++ 17-react-routing/src/pages/Products.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function ProductsPage() {
     return (
-        <h1>Products page</h1>
+        <>
+            <h1>Products page</h1>
+            <p>Go to <Link to="/">Home page</Link></p>
+        </>
     );
 }
