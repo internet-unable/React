e560a643efb75f99edb58feab3d540e1197cc220
internet-unable <k_moiseenko@icloud.com>
2024-12-18 13:45:08 +0200

    Update Home
    Example of how use useNavigate

--- 17-react-routing/src/pages/Home.js
+++ 17-react-routing/src/pages/Home.js
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+    const navigate = useNavigate();
+
+    function handleNavigate() {
+        navigate("/products");
+    }
+
     return (
         <>
             <h1>Home page</h1>
             <p>Go to <Link to="/products">Products page</Link></p>
+            <p>
+                <button onClick={handleNavigate}>Navigate to products page</button>
+            </p>
         </>
     );
 }
