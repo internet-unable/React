39cb4d92f9d841fa8ea3d0ef9a6566f59f2180cf
internet-unable <k_moiseenko@icloud.com>
2024-12-18 17:21:10 +0200

    Update EventDetail
    Output params

--- 18-react-routing-practice/frontend/src/pages/EventDetail.js
+++ 18-react-routing-practice/frontend/src/pages/EventDetail.js
@@ -1,5 +1,12 @@
+import { useParams } from "react-router-dom";
+
 export default function EventDetailPage() {
+    const params = useParams();
+
     return (
-        <h1>Event detail page</h1>
+        <>
+            <h1>Event detail page</h1>
+            {params.eventId}
+        </>
     );
 }
