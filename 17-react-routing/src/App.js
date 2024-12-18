8c8dd743b71a7cc86dd371ca77c8c593a052714d
internet-unable <k_moiseenko@icloud.com>
2024-12-18 12:21:54 +0200

    Apply navigation for app
    Create Root component and wrap application with it

--- 17-react-routing/src/App.js
+++ 17-react-routing/src/App.js
@@ -1,17 +1,36 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    // createRoutesFromElements,
+    // Route,
+} from "react-router-dom";
 
+import RootLayout from "./pages/Root/Root";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
 const router = createBrowserRouter([
-    { path: "/", element: <HomePage /> },
-    { path: "/products", element: <ProductsPage /> }
+    {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            { path: "/", element: <HomePage /> },
+            { path: "/products", element: <ProductsPage /> },
+        ],
+    },
 ]);
 
+// Option #2
+// const routeDefinitions = createRoutesFromElements(
+//     <Route>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/products" element={<ProductsPage />} />
+//     </Route>
+// );
+// const router = createBrowserRouter(routeDefinitions);
+
 function App() {
-    return (
-        <RouterProvider router={router} />
-    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
