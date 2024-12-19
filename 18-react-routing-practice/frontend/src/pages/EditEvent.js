4e5bce9fec8e170b3c98243e496162bbd3f8bf02
internet-unable <k_moiseenko@icloud.com>
2024-12-19 17:42:05 +0200

    Update EditEvent
    Use useRouteLoaderData

--- 18-react-routing-practice/frontend/src/pages/EditEvent.js
+++ 18-react-routing-practice/frontend/src/pages/EditEvent.js
@@ -1,5 +1,14 @@
+import { useRouteLoaderData  } from "react-router-dom";
+
+import EventForm from "../components/EventForm";
+
 export default function EditEventPage() {
+    const data = useRouteLoaderData('event-detail');
+
     return (
-        <h1>Edit event page</h1>
+        <>
+            <h1>Edit event page</h1>
+            <EventForm event= {data.event} />
+        </>
     );
 }
