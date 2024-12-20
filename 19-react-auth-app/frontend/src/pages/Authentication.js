0edc7d286eaea48151e2dc9f5824fafbb2b093d1
internet-unable <k_moiseenko@icloud.com>
2024-12-20 19:28:15 +0200

    Update App, MainNavigation and Authentication
    Update file indent.
    Add authAction. Add new route, for auth page. Add naviation ling, for auth page

--- 19-react-auth-app/frontend/src/pages/Authentication.js
+++ 19-react-auth-app/frontend/src/pages/Authentication.js
@@ -1,7 +1,42 @@
-import AuthForm from '../components/AuthForm';
+import { redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
-function AuthenticationPage() {
-  return <AuthForm />;
+export default function AuthenticationPage() {
+    return <AuthForm />;
 }
 
-export default AuthenticationPage;
+export async function authAction({ request }) {
+    const searchParams = new URL(request.url).searchParams;
+    const mode = searchParams.get("mode") || "login";
+    const data = await request.formData();
+    const autData = {
+        email: data.get("email"),
+        password: data.get("password"),
+    };
+
+    if (mode !== "login" && mode !== "signup") {
+        // or some other action
+        throw new Response(JSON.stringify({ message: "Unsupported mode" }), {
+            status: 422,
+        });
+    }
+
+    const response = await fetch("http://localhost:8080/" + mode, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(autData),
+    });
+
+    if (response.ok) {
+        return redirect("/");
+    } else if (response.status === 422 || response.status === 401) {
+        return response;
+    } else {
+        throw new Response(
+            JSON.stringify({ message: "Could not authenticate user" }),
+            { status: 500 }
+        );
+    }
+}
