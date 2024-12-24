98016369ecb03d86ce1d4bb39f11791e790db5cc
internet-unable <k_moiseenko@icloud.com>
2024-12-24 18:21:06 +0200

    Update EditEvent
    Add useQuery and useMutation

--- 21-react-tanstack-query/src/components/Events/EditEvent.jsx
+++ 21-react-tanstack-query/src/components/Events/EditEvent.jsx
@@ -1,27 +1,62 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 
+import { fetchEvent, updateEvent } from '../../utils/http.js';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
-  const navigate = useNavigate();
+    const params = useParams();
+    const navigate = useNavigate();
+    const { data, isPending, isError, error } = useQuery({
+        queryKey: ['events', params.id],
+        queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+    });
+    const { mutate } = useMutation({
+        mutationFn: updateEvent
+    });
 
-  function handleSubmit(formData) {}
+    function handleSubmit(formData) {
+        mutate({ id: params.id, event: formData });
+        navigate('../');
+    }
 
-  function handleClose() {
-    navigate('../');
-  }
+    function handleClose() {
+        navigate('../');
+    }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
-    </Modal>
-  );
+    return (
+        <Modal onClose={handleClose}>
+            {isPending && (
+                <div className="center">
+                    <LoadingIndicator />
+                </div>
+            )}
+
+            {isError && (
+                <>
+                    <ErrorBlock
+                        title="Failed to load event"
+                        message={error.info?.message || "Please check your inputs and try again"}
+                    />
+                    <div className="form-actions">
+                        <Link to="../" className="button">Okay</Link>
+                    </div>
+                </>
+            )}
+
+            {data && (
+                <EventForm inputData={data} onSubmit={handleSubmit}>
+                    <Link to="../" className="button-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="button">
+                        Update
+                    </button>
+                </EventForm>
+            )}
+        </Modal>
+    );
 }
