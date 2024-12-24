3e8f56d7e6d361cd68abab63ad3bfadb5c99aaf0
internet-unable <k_moiseenko@icloud.com>
2024-12-24 15:01:02 +0200

    Update NewEvent
    Update component with useMutation

--- 21-react-tanstack-query/src/components/Events/NewEvent.jsx
+++ 21-react-tanstack-query/src/components/Events/NewEvent.jsx
@@ -1,25 +1,44 @@
+import { useMutation } from "@tanstack/react-query";
 import { Link, useNavigate } from 'react-router-dom';
 
+import { createNewEvent } from '../../utils/http.js';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+    const { mutate, isPending, isError, error } = useMutation({
+        mutationFn: createNewEvent,
+        onSuccess: () => navigate('/events')
+    });
 
-  return (
-    <Modal onClose={() => navigate('../')}>
-      <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
-      </EventForm>
-    </Modal>
-  );
+    function handleSubmit(formData) {
+        mutate({ event: formData });
+    }
+
+    return (
+        <Modal onClose={() => navigate('../')}>
+            <EventForm onSubmit={handleSubmit}>
+                {isPending && <p>Submitting...</p>}
+                {!isPending && (
+                    <>
+                        <Link to="../" className="button-text">
+                            Cancel
+                        </Link>
+                        <button type="submit" className="button">
+                            Create
+                        </button>
+                    </>
+                )}
+            </EventForm>
+            {isError && (
+                <ErrorBlock
+                    title="Failed to create event"
+                    message={error.info?.message || "Failed to create event. Please check your inputs and try again"}
+                />
+            )}
+        </Modal>
+    );
 }
