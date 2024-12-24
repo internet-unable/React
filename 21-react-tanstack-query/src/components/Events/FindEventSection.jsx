bae8239c73f43a8a9ed2406486bfced172aaa947
internet-unable <k_moiseenko@icloud.com>
2024-12-24 12:53:21 +0200

    Update FindEventSection
    Add enabled to useQuery

--- 21-react-tanstack-query/src/components/Events/FindEventSection.jsx
+++ 21-react-tanstack-query/src/components/Events/FindEventSection.jsx
@@ -8,10 +8,11 @@ import EventItem from './EventItem.jsx';
 
 export default function FindEventSection() {
     const searchElement = useRef();
-    const [searchTerm, setSearchTerm] = useState('');
-    const { data, isPending, isError, error } = useQuery({
+    const [searchTerm, setSearchTerm] = useState();
+    const { data, isLoading, isError, error } = useQuery({
         queryKey: ["events", { search: searchTerm }],
-        queryFn: () => fetchEvents(searchTerm)
+        queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }),
+        enabled: searchTerm !== undefined
     });
     let content = <p>Please enter a search term and to find events.</p>;
 
@@ -20,7 +21,7 @@ export default function FindEventSection() {
         setSearchTerm(searchElement.current.value);
     }
 
-    if (isPending) {
+    if (isLoading) {
         content = <LoadingIndicator />;
     }
 
