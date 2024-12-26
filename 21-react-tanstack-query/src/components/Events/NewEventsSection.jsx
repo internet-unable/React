735aa212f624f3a8d8123a7d26d553a248de86ef
internet-unable <k_moiseenko@icloud.com>
2024-12-26 15:06:28 +0200

    Update http.js, NewEventsSection, FindEventSection
    Update fetchEvents, to recieve max param.

--- 21-react-tanstack-query/src/components/Events/NewEventsSection.jsx
+++ 21-react-tanstack-query/src/components/Events/NewEventsSection.jsx
@@ -7,42 +7,9 @@ import EventItem from './EventItem.jsx';
 import { fetchEvents } from "../../utils/http";
 
 export default function NewEventsSection() {
-    // const [data, setData] = useState();
-    // const [error, setError] = useState();
-    // const [isLoading, setIsLoading] = useState(false);
-
-    // useEffect(() => {
-    //     async function fetchEvents() {
-    //         setIsLoading(true);
-    //         const response = await fetch('http://localhost:3000/events');
-
-    //         if (!response.ok) {
-    //             const error = new Error('An error occurred while fetching the events');
-    //             error.code = response.status;
-    //             error.info = await response.json();
-    //             throw error;
-    //         }
-
-    //         const { events } = await response.json();
-
-    //         return events;
-    //     }
-
-    //     fetchEvents()
-    //         .then((events) => {
-    //             setData(events);
-    //         })
-    //         .catch((error) => {
-    //             setError(error);
-    //         })
-    //         .finally(() => {
-    //             setIsLoading(false);
-    //         });
-    // }, []);
-
     const { data, isPending, isError, error } = useQuery({
-        queryKey: ["events"],
-        queryFn: fetchEvents
+        queryKey: ["events", { max: 3 }],
+        queryFn: ({ signal, queryKey }) => fetchEvents({ signal, ...queryKey[1] })
     });
     let content;
 
