147969d7da15fc92583e94070c3a71c2bc9b2a51
internet-unable <k_moiseenko@icloud.com>
2025-01-20 19:08:48 +0200

    Update app/pages
    - apply DataFetchingDemo, to see how server component fetch data;

--- 24-react-rsc_server-actions_use/app/page.js
+++ 24-react-rsc_server-actions_use/app/page.js
@@ -1,14 +1,17 @@
 import RSCDemo from "@/components/RSCDemo";
 import ClientDemo from "@/components/ClientDemo";
+import DataFetchingDemo from "@/components/DataFetchingDemo";
 
 export default function Home() {
     return (
         <main>
-            <RSCDemo /> 
-            
-            <ClientDemo>
-                <RSCDemo /> 
-            </ClientDemo>
+            {/* <RSCDemo /> */}
+
+            {/* <ClientDemo>
+                <RSCDemo />
+            </ClientDemo> */}
+
+            <DataFetchingDemo />
         </main>
     );
 }
