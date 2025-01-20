b43fd1899c48bb9c78d248ce510a68b0b55c656b
internet-unable <k_moiseenko@icloud.com>
2025-01-20 20:52:03 +0200

    Update app/page
    - apply server action;

--- 24-react-rsc_server-actions_use/app/page.js
+++ 24-react-rsc_server-actions_use/app/page.js
@@ -1,6 +1,7 @@
 import RSCDemo from "@/components/RSCDemo";
 import ClientDemo from "@/components/ClientDemo";
 import DataFetchingDemo from "@/components/DataFetchingDemo";
+import ServerActionsDemo from "@/components/ServerActionsDemo";
 
 export default function Home() {
     return (
@@ -11,7 +12,9 @@ export default function Home() {
                 <RSCDemo />
             </ClientDemo> */}
 
-            <DataFetchingDemo />
+            {/* <DataFetchingDemo /> */}
+
+            <ServerActionsDemo />
         </main>
     );
 }
