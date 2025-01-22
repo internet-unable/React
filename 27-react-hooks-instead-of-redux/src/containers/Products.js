68774892f1ddb900cb82c7c46507d87f3cf11dbe
internet-unable <k_moiseenko@icloud.com>
2025-01-22 23:24:24 +0200

    Replace Redux with Context
     - create ProductsContext and wrap app with ProductsProvider;
    - apply ProductsContext for Products;

--- 27-react-hooks-instead-of-redux/src/containers/Products.js
+++ 27-react-hooks-instead-of-redux/src/containers/Products.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from "react";
+// import { useSelector } from "react-redux";
 
-import ProductItem from '../components/Products/ProductItem';
-import './Products.css';
+import ProductItem from "../components/Products/ProductItem";
+import { ProductsContext } from "../context/products-context";
+import "./Products.css";
 
-const Products = props => {
-  const productList = useSelector(state => state.shop.products);
-  return (
-    <ul className="products-list">
-      {productList.map(prod => (
-        <ProductItem
-          key={prod.id}
-          id={prod.id}
-          title={prod.title}
-          description={prod.description}
-          isFav={prod.isFavorite}
-        />
-      ))}
-    </ul>
-  );
+const Products = (props) => {
+    // const productList = useSelector((state) => state.shop.products);
+    const { productsList } = useContext(ProductsContext);
+
+    return (
+        <ul className="products-list">
+            {productsList.map((prod) => (
+                <ProductItem
+                    key={prod.id}
+                    id={prod.id}
+                    title={prod.title}
+                    description={prod.description}
+                    isFav={prod.isFavorite}
+                />
+            ))}
+        </ul>
+    );
 };
 
 export default Products;
