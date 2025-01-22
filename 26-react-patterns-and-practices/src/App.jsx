d94fcd28b2523b037126380e5f6346567c8e7b68
internet-unable <k_moiseenko@icloud.com>
2025-01-22 15:49:43 +0200

    Update App
    - add id's for AccordionItem

--- 26-react-patterns-and-practices/src/App.jsx
+++ 26-react-patterns-and-practices/src/App.jsx
@@ -7,14 +7,22 @@ export default function App() {
             <section>
                 <h2>Why work with us?</h2>
                 <Accordion className="accordion">
-                    <AccordionItem className="accordion-item" title="We got 20 years of experiece">
+                    <AccordionItem
+                        id="experience"
+                        className="accordion-item"
+                        title="We got 20 years of experiece"
+                    >
                         <article>
                             <p>You can&apos;t go wrong with us</p>
                             <p>We are in the business of planning trips for more then 20 years</p>
                         </article>
                     </AccordionItem>
 
-                    <AccordionItem className="accordion-item" title="We're working with local guides">
+                    <AccordionItem
+                        id="local-guides"
+                        className="accordion-item"
+                        title="We're working with local guides"
+                    >
                         <article>
                             <p>We are not doing it along from out office</p>
                             <p>Instead we are working with local guides to ensure a safe and pleasent vacation</p>
