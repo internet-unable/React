fd0429f43af5ca33e77f5706ddc8e350f67fb645
internet-unable <k_moiseenko@icloud.com>
2025-01-22 23:50:09 +0200

    Update Favorities
    - apply Context, to render favoriteProducts;

--- 27-react-hooks-instead-of-redux/src/containers/Favorites.js
+++ 27-react-hooks-instead-of-redux/src/containers/Favorites.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from "react";
+// import { useSelector } from "react-redux";
 
-import FavoriteItem from '../components/Favorites/FavoriteItem';
-import './Products.css';
+import { ProductsContext } from "../context/products-context";
+import FavoriteItem from "../components/Favorites/FavoriteItem";
+import "./Products.css";
 
-const Favorites = props => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
-  );
-  let content = <p className="placeholder">Got no favorites yet!</p>;
-  if (favoriteProducts.length > 0) {
-    content = (
-      <ul className="products-list">
-        {favoriteProducts.map(prod => (
-          <FavoriteItem
-            key={prod.id}
-            id={prod.id}
-            title={prod.title}
-            description={prod.description}
-          />
-        ))}
-      </ul>
-    );
-  }
-  return content;
+const Favorites = (props) => {
+    // const favoriteProducts = useSelector((state) =>
+    //     state.shop.products.filter((p) => p.isFavorite)
+    // );
+    
+    const favoriteProducts = useContext(ProductsContext).productsList.filter(item => item.isFavorite);
+    let content = <p className="placeholder">Got no favorites yet!</p>;
+
+    if (favoriteProducts.length > 0) {
+        content = (
+            <ul className="products-list">
+                {favoriteProducts.map((prod) => (
+                    <FavoriteItem
+                        key={prod.id}
+                        id={prod.id}
+                        title={prod.title}
+                        description={prod.description}
+                    />
+                ))}
+            </ul>
+        );
+    }
+    return content;
 };
 
 export default Favorites;
