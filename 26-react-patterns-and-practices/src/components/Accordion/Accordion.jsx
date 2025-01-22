7106ebca7e86d8c980f314f0ccce311b25fda582
internet-unable <k_moiseenko@icloud.com>
2025-01-22 15:48:43 +0200

    Update Accordion
    - add context;
    - add useAccorionContext that return context;

--- 26-react-patterns-and-practices/src/components/Accordion/Accordion.jsx
+++ 26-react-patterns-and-practices/src/components/Accordion/Accordion.jsx
@@ -1,7 +1,39 @@
+import { createContext, useContext, useState } from "react";
+
+const AccorionContext = createContext();
+
+export function useAccorionContext() {
+    const ctx = useContext(AccorionContext);
+
+    if (!ctx) {
+        throw new Error("Accortdion related components must be wrapped by <Accordion>");
+    }
+
+    return ctx;
+}
+
 export default function Accordion({ children, className }) {
+    const [openItemId, setOpenItemId] = useState();
+
+    const contextValue = {
+        openItemId,
+        openItem,
+        closeItem
+    };
+
+    function openItem(id) {
+        setOpenItemId(id);
+    }
+
+    function closeItem() {
+        setOpenItemId(null);
+    }
+
     return (
-        <ul className={className}>
-            {children}
-        </ul>
+        <AccorionContext.Provider value={contextValue}>
+            <ul className={className}>
+                {children}
+            </ul>
+        </AccorionContext.Provider>
     );
 }
