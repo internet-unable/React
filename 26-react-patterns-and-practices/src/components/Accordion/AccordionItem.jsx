8f5fa28ed087cabc2b031cb104e711cffa34c512
internet-unable <k_moiseenko@icloud.com>
2025-01-22 15:49:24 +0200

    Update AccordionItem
    - apply context from Accordion;

--- 26-react-patterns-and-practices/src/components/Accordion/AccordionItem.jsx
+++ 26-react-patterns-and-practices/src/components/Accordion/AccordionItem.jsx
@@ -1,8 +1,21 @@
-export default function AccordionItem({ className, title, children }) {
+import { useAccorionContext } from "./Accordion.jsx"
+
+export default function AccordionItem({ id, className, title, children }) {
+    const { openItemId, openItem, closeItem } = useAccorionContext();
+    const isOpen = openItemId === id;
+
+    function handleClick() {
+        if (isOpen) {
+            closeItem();
+        } else {
+            openItem(id);
+        }
+    }
+
     return (
         <li className={className}>
-            <h3>{title}</h3>
-            <div>{children}</div>
+            <h3 onClick={handleClick}>{title}</h3>
+            <div className={isOpen ? "accordion-item-content open" : "accordion-item-content"}>{children}</div>
         </li>
     );
 }
