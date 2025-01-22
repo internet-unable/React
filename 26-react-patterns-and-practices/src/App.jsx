b2c4e35c034dec5c3374a7bc999d7068f0064ed3
internet-unable <k_moiseenko@icloud.com>
2025-01-22 17:06:11 +0200

    Update App
    - apply Accordion changes;

--- 26-react-patterns-and-practices/src/App.jsx
+++ 26-react-patterns-and-practices/src/App.jsx
@@ -1,4 +1,5 @@
 import Accordion from "./components/Accordion/Accordion.jsx";
+import AccordionContent from "./components/Accordion/AccordionContent.jsx";
 
 export default function App() {
     return (
@@ -6,26 +7,28 @@ export default function App() {
             <section>
                 <h2>Why work with us?</h2>
                 <Accordion className="accordion">
-                    <Accordion.Item
-                        id="experience"
-                        className="accordion-item"
-                        title="We got 20 years of experiece"
-                    >
-                        <article>
-                            <p>You can&apos;t go wrong with us</p>
-                            <p>We are in the business of planning trips for more then 20 years</p>
-                        </article>
+                    <Accordion.Item className="accordion-item">
+                        <Accordion.Title id="experience" className="accordion-item-title">
+                            We got 20 years of experiece
+                        </Accordion.Title>
+                        <AccordionContent id="experience" className="accordion-item-content">
+                            <article>
+                                <p>You can&apos;t go wrong with us</p>
+                                <p>We are in the business of planning trips for more then 20 years</p>
+                            </article>
+                        </AccordionContent>
                     </Accordion.Item>
 
-                    <Accordion.Item
-                        id="local-guides"
-                        className="accordion-item"
-                        title="We're working with local guides"
-                    >
-                        <article>
-                            <p>We are not doing it along from out office</p>
-                            <p>Instead we are working with local guides to ensure a safe and pleasent vacation</p>
-                        </article>
+                    <Accordion.Item className="accordion-item">
+                        <Accordion.Title id="local-guides" className="accordion-item-title">
+                            We&apos;re working with local guides
+                        </Accordion.Title>
+                        <Accordion.Content id="local-guides" className="accordion-item-content">
+                            <article>
+                                <p>We are not doing it along from out office</p>
+                                <p>Instead we are working with local guides to ensure a safe and pleasent vacation</p>
+                            </article>
+                        </Accordion.Content>
                     </Accordion.Item>
                 </Accordion>
             </section>
