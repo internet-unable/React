afbb06e912c19bee8e9c4c1bea9de3009e2a0aef
internet-unable <k_moiseenko@icloud.com>
2025-01-24 18:17:29 +0200

    Update Greeting
    - apply useState;

--- 28-react-tests/src/components/Greeting.js
+++ 28-react-tests/src/components/Greeting.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 export default function Greeting() {
+    const [changedText, setChangedText] = useState(false);
+
+    function handleTextChange() {
+        setChangedText(true);
+    }
+
     return (
         <>
             <h2>Hello world!</h2>
-            <p>It is good to see you</p>
+            {/* <p>It is good to see you</p> */}
+            {!changedText && <p>It is good to see you</p>}
+            {changedText && <p>Changed!</p>}
+            <button onClick={handleTextChange}>Change text</button>
         </>
     );
 }
