972e17ba1e1bb62e051e93e6f90e9fc8c233e399
internet-unable <k_moiseenko@icloud.com>
2025-01-24 18:17:47 +0200

    Update Greeting.test
    - apply couple tests;

--- 28-react-tests/src/components/Greeting.test.js
+++ 28-react-tests/src/components/Greeting.test.js
@@ -1,14 +1,59 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
-test("testing Greeting component", () => {
-    // Arrange
-    render(<Greeting />);
+describe("Greeting component", () => {
+    test("renders 'Hello world!' as a title", () => {
+        // Arrange
+        render(<Greeting />);
 
-    // Act
-    // Nothing in this case
+        // Act
+        // Nothing in this case
 
-    // Assert
-    const element = screen.getByText("Hello world!");
-    expect(element).toBeInTheDocument();
+        // Assert
+        const element = screen.getByText("Hello world!");
+        expect(element).toBeInTheDocument();
+    });
+
+    test("renders 'It is good to see you' if the button was NOT clicked", () => {
+        // Arrange
+        render(<Greeting />);
+
+        // Act
+        // Nothing in this case
+
+        // Assert
+        const element = screen.getByText("It is good to see you", {
+            exact: false,
+        });
+        expect(element).toBeInTheDocument();
+    });
+
+    test("renders 'Changed!' is the button was clicked", () => {
+        // Arrange
+        render(<Greeting />);
+
+        // Act
+        const button = screen.getByRole("button");
+        userEvent.click(button);
+
+        // Assert
+        const element = screen.getByText("Changed!");
+        expect(element).toBeInTheDocument();
+    });
+
+    test("does'nt render 'It is good to see you' if the button was clicked", () => {
+        // Arrange
+        render(<Greeting />);
+
+        // Act
+        const button = screen.getByRole("button");
+        userEvent.click(button);
+
+        // Assert
+        const element = screen.queryByText("It is good to see you", {
+            exact: false,
+        });
+        expect(element).toBeNull();
+    });
 });
