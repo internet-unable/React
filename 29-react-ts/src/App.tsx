da92f3ca1f332b1f9ea0e5229302c5d114f684c8
internet-unable <k_moiseenko@icloud.com>
2025-01-27 22:10:48 +0200

    Apply Todo type
    - apply Todo type for Todos component;
    - apply Todo type for data in App;

--- 29-react-ts/src/App.tsx
+++ 29-react-ts/src/App.tsx
@@ -1,10 +1,17 @@
+import Todo from "./models/todo";
+
 import Todos from "./components/Todos";
 import "./App.css";
 
 export default function App() {
+    const todos = [
+        new Todo("Learn React"),
+        new Todo("Learn TypeScript")
+    ];
+
     return (
         <div className="App">
-            <Todos todos={["Learn React", "Learn TypeScript"]} />
+            <Todos todos={todos} />
         </div>
     );
 }
--- 29-react-ts/src/components/Todos.tsx
+++ 29-react-ts/src/components/Todos.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Todo from "../models/todo";
 
-const Todos: React.FC<{todos: string[]}> = (props) => {
+const Todos: React.FC<{todos: Todo[]}> = (props) => {
     return (
         <ul>
-            {props.todos.map(todo => <li>{todo}</li>)}
+            {props.todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
         </ul>
     );
 }
