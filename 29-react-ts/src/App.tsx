0acf975a4ec3928b525160b36c856bbd269b0d99
internet-unable <k_moiseenko@icloud.com>
2025-01-28 19:04:33 +0200

    Update App
    - apply useState;
    - add handleAddTodo;
    - pass handleAddTodo to NewTodo;

--- 29-react-ts/src/App.tsx
+++ 29-react-ts/src/App.tsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
+
 import Todo from "./models/todo";
 
 import Todos from "./components/Todos";
+import NewTodo from "./components/NewTodo";
 import "./App.css";
 
 export default function App() {
-    const todos = [
+    const [todos, setTodos] = useState<Todo[]>([
         new Todo("Learn React"),
         new Todo("Learn TypeScript")
-    ];
+    ]);
+
+    const handleAddTodo = (value: string) => {
+        const newTodo = new Todo(value);
+        setTodos(prevTodos => prevTodos.concat(newTodo));
+    }
 
     return (
         <div className="App">
+            <NewTodo onAddTodo={handleAddTodo} />
             <Todos todos={todos} />
         </div>
     );
