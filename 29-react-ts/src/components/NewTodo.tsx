62994b546495a459f3eebc21debf11e3c12189d1
internet-unable <k_moiseenko@icloud.com>
2025-01-28 19:03:57 +0200

    Apply styles for Todos, TodoItem, NewTodo

--- 29-react-ts/src/components/NewTodo.tsx
+++ 29-react-ts/src/components/NewTodo.tsx
@@ -1,5 +1,7 @@
 import React, { useRef } from "react";
 
+import classes from "./NewTodo.module.css";
+
 const NewTodo: React.FC<{onAddTodo: (inputValue: string) => void}> = (props) => {
     const input = useRef<HTMLInputElement>(null);
 
@@ -15,7 +17,7 @@ const NewTodo: React.FC<{onAddTodo: (inputValue: string) => void}> = (props) => 
     };
 
     return (
-        <form onSubmit={handleFormSubmit}>
+        <form className={classes.form} onSubmit={handleFormSubmit}>
             <label htmlFor="new-todo">Todo text</label>
             <input type="text" id="new-todo" ref={input} />
             <button>Add todo</button>
--- 29-react-ts/src/components/Todos.tsx
+++ 29-react-ts/src/components/Todos.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Todo from "../models/todo";
 
 import TodoItem from "./TodoItem";
+import classes from "./Todos.module.css";
 
 const Todos: React.FC<{todos: Todo[]}> = (props) => {
     return (
-        <ul>
+        <ul className={classes.todos}>
             {props.todos.map(todo => <TodoItem key={todo.id} text={todo.text} />)}
         </ul>
     );
