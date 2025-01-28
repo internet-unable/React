0af5e7da438f776fb3ab44c243314f8b8f971b8a
internet-unable <k_moiseenko@icloud.com>
2025-01-28 18:41:17 +0200

    Create TodoItem
    - create TodoItem and apply it in Todos;

--- 29-react-ts/src/components/Todos.tsx
+++ 29-react-ts/src/components/Todos.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Todo from "../models/todo";
 
+import TodoItem from "./TodoItem";
+
 const Todos: React.FC<{todos: Todo[]}> = (props) => {
     return (
         <ul>
-            {props.todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+            {props.todos.map(todo => <TodoItem key={todo.id} text={todo.text} />)}
         </ul>
     );
 }
